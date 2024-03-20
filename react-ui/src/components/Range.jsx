fcadef1c74174229a5960e81387bf62136b0c079
Daniel Harte <norgeous@users.noreply.github.com>
2024-03-20 15:08:17 +0000

    uikit icons

--- react-ui/src/components/Range.jsx
+++ react-ui/src/components/Range.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/no-unknown-property */
-const Range = ({ options, loading, label, info, minLabel, maxLabel, ...props }) => (
+const Range = ({
+  options,
+  loading,
+  label,
+  info,
+  minLabel,
+  maxLabel,
+  ...props
+}) => (
   <label className="uk-form-label">
     {label}
     {info && (
       <>
         {' '}
-        <button uk-icon="icon: question" type="button">[?]</button>
+        <button uk-icon="icon: question" type="button"></button>
         <div className="uk-card uk-card-small uk-card-body uk-card-default" uk-drop="pos: right-top; mode: click;">
           {info}
         </div>
@@ -19,16 +27,18 @@ const Range = ({ options, loading, label, info, minLabel, maxLabel, ...props }) 
       step="1"
       {...props}
     />
-    <div className="uk-form-label uk-grid-small uk-text-muted" uk-grid>
-      <div className="uk-width-1-2@s">
-        <span uk-icon="icon: arrow-left"></span>
-        {minLabel}
+    {minLabel && maxLabel && (
+      <div className="uk-form-label uk-grid-small uk-text-muted uk-flex uk-flex-between">
+        <div>
+          <span uk-icon="icon: arrow-left"></span>
+          {minLabel}
+        </div>
+        <div>
+          {maxLabel}
+          <span uk-icon="icon: arrow-right"></span>
+        </div>
       </div>
-      <div className="uk-width-1-2@s" style={{textAlign:'right'}}>
-        {maxLabel}
-        <span uk-icon="icon: arrow-right"></span>
-      </div>
-    </div>
+    )}
   </label>
 );
 
--- react-ui/src/main.jsx
+++ react-ui/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 import { AppProvider } from './contexts/AppContext';
 import App from './App'
+
 import 'uikit/dist/css/uikit.min.css';
 import './index.css';
 import './dungeon.css';
 
+UIkit.use(Icons);
 window.UIkit = UIkit;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
