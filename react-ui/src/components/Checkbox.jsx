c6b896fa740853b5e5e635361f25bb2c017a5136
norgeous <daniel@harte.me>
2024-03-26 17:31:47 +0000

    tidy

--- react-ui/src/components/Checkbox.jsx
+++ react-ui/src/components/Checkbox.jsx
@@ -4,7 +4,7 @@ const Checkbox = ({ name, label, value, onChange, ...props }) => {
   const handleChange = (event) => onChange({ [name]: Boolean(event.currentTarget.checked) });
 
   return (
-    <label className="uk-form-label" style={{ display: 'block', marginTop: 30 }}>
+    <label className="uk-form-label" style={{ display: 'block', marginTop: 32 }}>
       <input
         {...props}
         className="uk-checkbox"
