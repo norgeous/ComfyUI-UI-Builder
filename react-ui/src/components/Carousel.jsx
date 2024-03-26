14d1ad9a951313df2b4f390d0a67b4ce495d3f6f
norgeous <daniel@harte.me>
2024-03-26 22:32:50 +0000

    Update Carousel.jsx

--- react-ui/src/components/Carousel.jsx
+++ react-ui/src/components/Carousel.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const ImagesContainer = styled.section`
   padding: 20px;
   display: grid;
-  grid-template-rows: repeat(${({ size }) => size}, 1fr);
+  /* grid-template-rows: repeat(${({ size }) => size}, 1fr); */
   grid-template-columns: repeat(${({ size }) => size}, 1fr);
   gap: 20px;
 `;
@@ -15,7 +15,7 @@ const Carousel = ({ images }) => {
       {images.map(({ filename, subfolder }) => (
         <img
           key={filename}
-          style={{ display: 'block', aspectRatio: 1, width: '100%', maxWidth: 512 }}
+          style={{ display: 'block', aspectRatio: 1, width: '100%' }}
           src={`http://127.0.0.1:8188/view?filename=${filename}&type=output&subfolder=${subfolder}`}
           alt=""
         />
