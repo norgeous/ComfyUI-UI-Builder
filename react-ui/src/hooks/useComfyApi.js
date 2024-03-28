995587752b29fe2c37b157b4705bca89d5cd64e8
norgeous <daniel@harte.me>
2024-03-28 13:58:31 +0000

    Update useComfyApi.js

--- react-ui/src/hooks/useComfyApi.js
+++ react-ui/src/hooks/useComfyApi.js
@@ -25,13 +25,22 @@ const useComfyApi = ({
       })
         .then(async (res) => {
           setLoading(false);
+
+          const resType = res.headers.get('content-type').split(';')[0];
+
+          if (res.status !== 200 && resType === 'text/plain') {
+            setError(await res.text());
+            return;
+          } 
+
           const json = await res.json();
           if (res.status !== 200) setError(json.node_errors['1'].errors[0].details);
           return json;
         })
         .then((res) => setData(adapter(res)))
         .catch((err) => {
-          console.error(err);  
+          console.error('error in useComfyApi chain');
+          console.error(err);
           setLoading(false);
           setError(err.message);
         });
