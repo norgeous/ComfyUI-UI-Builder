236e018a521d5626381eaabf9c3f3a5c436eb96a
norgeous <daniel@harte.me>
2024-03-29 19:32:24 +0000

    refactor to json

---
+++ react-ui/src/configs/index.js
@@ -0,0 +1,9 @@
+import dungeon from './dungeon-character-portrait/index.json';
+import example2 from './example2/index.json';
+
+const configList = [
+  dungeon,
+  example2,
+];
+
+export default configList;
