849b895018582ec55c12e64dfa1ef5d1e7133770
norgeous <daniel@harte.me>
2024-04-03 20:13:29 +0100

    x

--- react-ui/src/configs/index.js
+++ react-ui/src/configs/index.js
@@ -1,10 +1,13 @@
-import dungeonConfig from './dungeon-character-portrait/index.json';
-import dungeonWorkflow from './dungeon-character-portrait/basic_portrait_lcm_bypassed-workflow.json';
+import dungeonConfig from './dungeon-character-portrait/ui-config.json';
+import dungeonWorkflow from './dungeon-character-portrait/workflow.json';
 
 // import example2 from './example2/index.json';
 
 const configList = [
-  { configData: dungeonConfig, baseWorkflow: dungeonWorkflow },
+  {
+    configData: dungeonConfig,
+    baseWorkflow: dungeonWorkflow,
+  },
   // example2,
 ];
 
