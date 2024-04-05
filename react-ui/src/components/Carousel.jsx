477a0640fe553423fbc91b3de457f8755e1d2776
norgeous <daniel@harte.me>
2024-04-05 16:14:11 +0100

    Update Carousel.jsx

--- react-ui/src/components/Carousel.jsx
+++ react-ui/src/components/Carousel.jsx
@@ -1,42 +1,33 @@
 /* eslint-disable react/prop-types */
 import styled from 'styled-components';
 
-// const ImagesContainer = styled.section`
-//   width: 100%;
-//   height: 100%;
-// `;
+const Div = styled.div`
+  width: 100%;
+  height: 100%;
+  scroll-snap-align: center;
+  display: grid;
+  place-items: center;
+`;
 
 const Img = styled.img`
   display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: contain;  scroll-snap-align: center;
-  margin-bottom: 10px;
+  padding: 5px;
+  max-width: 100%;
+  max-height: 100%;
+  cursor: pointer;
 `;
 
 const Carousel = ({ images }) => {
-  const count = images.length || 5;
-  const cols = Math.ceil(Math.sqrt(count));
-  const rows = Math.ceil(count / cols);
-
   return (
     <>
       {images.map(({ filename, subfolder }) => (
-        <Img
-          key={filename}
-          src={`/view?filename=${filename}&type=output&subfolder=${subfolder}`}
-          alt=""
-        />
+        <Div key={filename}>
+          <Img
+            src={`/view?filename=${filename}&type=output&subfolder=${subfolder}`}
+            alt=""
+          />
+        </Div>
       ))}
-      <Img rows={rows} src="/view?filename=Dungeon_01426_.png" />
-      <Img rows={rows} src="/view?filename=Dungeon_01427_.png" />
-      <Img rows={rows} src="/view?filename=Dungeon_01428_.png" />
-      <Img rows={rows} src="/view?filename=Dungeon_01429_.png" />
-      <Img rows={rows} src="/view?filename=Dungeon_01430_.png" />
-      <Img rows={rows} src="/view?filename=Dungeon_01431_.png" />
-      <Img rows={rows} src="/view?filename=Dungeon_01432_.png" />
-      <Img rows={rows} src="/view?filename=Dungeon_01433_.png" />
-      <Img rows={rows} src="/view?filename=Dungeon_01434_.png" />
     </>
   );
 };
