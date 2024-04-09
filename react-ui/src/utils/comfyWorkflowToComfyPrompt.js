5ba1d52dc4f104b10b70def26a2721e3a58ffac7
norgeous <daniel@harte.me>
2024-04-09 23:12:42 +0100

    Update comfyWorkflowToComfyPrompt.js

--- react-ui/src/utils/comfyWorkflowToComfyPrompt.js
+++ react-ui/src/utils/comfyWorkflowToComfyPrompt.js
@@ -54,6 +54,7 @@ const comfyWorkflowToComfyPrompt = ({
     const newInputs = keys.reduce((acc2, key) => {
       const linkId = inputs?.find(({ name }) => name === key)?.link; // linkId or undefined
 
+      // eslint-disable-next-line camelcase
       const value = linkId ? getLink(linkLookup, linkId) : widgets_values.shift();
       return ({ ...acc2, [key]: value });
     }, {});
