e6cdecbcedc2b40b55a1bbb848787f47f23c1b81
norgeous <daniel@harte.me>
2024-04-09 20:37:45 +0100

    :knife: chop Debug.jsx

---
+++ react-ui/src/components/Debug.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
+const Pre = styled.pre`
+  position: absolute;
+  inset: 0;
+  white-space: pre-wrap;
+  background: #000e;
+  color: green;
+  padding: 40px;
+  margin: 0;
+  border: 0;
+  z-index: 10;
+`;
+
+const Debug = ({ label, data }) => {
+  const [open, setOpen] = useState(false);
+  const toggle = () => setOpen(!open);
+
+  return (
+    <>
+      <Button onClick={toggle} title={label}>🐞</Button>
+      {open && (
+        <Pre onClick={toggle}>
+          {JSON.stringify(data, null, 2)}
+        </Pre>
+      )}
+    </>
+  );
+};
+
+export default Debug;
--- react-ui/src/components/StatusBar.jsx
+++ react-ui/src/components/StatusBar.jsx
@@ -1,7 +1,6 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
 import styled from 'styled-components';
 import useAppContext from '../hooks/useAppContext';
+import Debug from './Debug';
 
 const Bar = styled.div`
   padding: 2px;
@@ -14,40 +13,6 @@ const Bar = styled.div`
   text-overflow: ellipsis;
   overflow-x: scroll;
 `;
-
-const Button = styled.button`
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
-`;
-
-const Pre = styled.pre`
-  position: absolute;
-  inset: 0;
-  white-space: pre-wrap;
-  background: #000e;
-  color: green;
-  padding: 40px;
-  margin: 0;
-  border: 0;
-  z-index: 10;
-`;
-
-const Debug = ({ label, data }) => {
-  const [open, setOpen] = useState(false);
-  const toggle = () => setOpen(!open);
-
-  return (
-    <>
-      <Button onClick={toggle} title={label}>🐞</Button>
-      {open && (
-        <Pre onClick={toggle}>
-          {JSON.stringify(data, null, 2)}
-        </Pre>
-      )}
-    </>
-  );
-};
 
 const StatusBar = () => {
   const {
