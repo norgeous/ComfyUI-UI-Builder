ba4de8021d9d0a925e730e4d1afb5c55ccaa93fc
norgeous <daniel@harte.me>
2024-05-01 00:23:42 +0100

    Update Missing.jsx

--- react-ui/src/components/OLD/form-fields/Missing.jsx
+++ react-ui/src/components/OLD/form-fields/Missing.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
 const Missing = ({ type = undefined }) => (
-  <>
+  <div>
     component type <strong>{type}</strong> not found
-  </>
+  </div>
 );
 
 Missing.propTypes = {
