9db60d0599a6f2c9e3fcf5f7cc5cae1e5f21ff7e
norgeous <daniel@harte.me>
2024-05-04 18:06:30 +0100

    Update useComfyPrompt.js

--- react-ui/src/hooks/useComfyPrompt.js
+++ react-ui/src/hooks/useComfyPrompt.js
@@ -1,13 +1,17 @@
+import WsContext from '@/contexts/WsContext';
+import { useContext } from 'react';
 import useComfyApi from './useComfyApi';
 
-const useComfyPrompt = ({ bodyData }) =>
-  useComfyApi({
-    fetchUrl: '/prompt',
+const useComfyPrompt = ({ bodyData }) => {
+  const { comfyUrl } = useContext(WsContext);
+  return useComfyApi({
+    fetchUrl: `${comfyUrl}/prompt`,
     options: {
       method: 'POST',
       body: JSON.stringify(bodyData),
     },
     enabled: false,
   });
+};
 
 export default useComfyPrompt;
