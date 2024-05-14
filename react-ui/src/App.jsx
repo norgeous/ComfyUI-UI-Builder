91d5945b8f21aca8ba8ddf9923f5dc0130f3b990
norgeous <daniel@harte.me>
2024-05-14 18:36:51 +0100

    Update App.jsx

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -6,11 +6,15 @@ import UiSelector from './components/UiSelector';
 import FormBuilder from './components/FormBuilder';
 import FormControls from './components/FormControls';
 import Progress from './components/Progress/Progress';
-import Carousel from './components/Carousel/Carousel';
+// import Carousel from './components/Carousel/Carousel';
 import StatusBar from './components/StatusBar';
+import ImageGrid from './components/ImageGrid/ImageGrid';
 
 const App = () => {
-  const { progress } = useContext(WsContext);
+  const { comfyUrl, progress, output } = useContext(WsContext);
+  const images = (output?.images || []).map(
+    ({ filename }) => `${comfyUrl}/view?type=output&filename=${filename}`,
+  );
 
   return (
     <>
@@ -20,7 +24,8 @@ const App = () => {
         <Main>
           <Progress value={progress} />
           <Content>
-            <Carousel />
+            {/* <Carousel /> */}
+            <ImageGrid images={images} />
           </Content>
           <StatusBar />
         </Main>
