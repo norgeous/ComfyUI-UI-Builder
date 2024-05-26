2ed7a13b2df9ae8cdf738678a205be57ae28bfae
norgeous <daniel@harte.me>
2024-05-26 21:54:16 +0100

    Update useSpeech.js

--- react-ui/src/hooks/useSpeech.js
+++ react-ui/src/hooks/useSpeech.js
@@ -14,7 +14,7 @@ const useSpeech = () => {
     .join('. ');
   const simpleOutput = `${simpleUtterances}. ${partial}`;
 
-  const tail = simpleOutput.split(' ').slice(-30).join(' ');
+  const tail = simpleOutput.split(' ').slice(-10).join(' ');
 
   return { loading, muted, toggleMic, utterances, partial, tail };
 };
