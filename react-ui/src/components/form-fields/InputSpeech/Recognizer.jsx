db30e4708ac35a309ad5fbc5d6f03288909b3233
norgeous <daniel@harte.me>
2024-05-26 12:40:58 +0100

    move logic into hook

--- react-ui/src/components/form-fields/InputSpeech/Recognizer.jsx
+++ react-ui/src/components/form-fields/InputSpeech/Recognizer.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
 import styled from 'styled-components';
-import { createModel } from 'vosk-browser';
+import useVosk from '@/hooks/useVosk';
 import Microphone from './microphone';
 import ModelLoader from './model-loader';
 
@@ -35,37 +34,16 @@ const Word = styled.span`
 `;
 
 const Recognizer = () => {
-  const [utterances, setUtterances] = useState([]);
-  const [partial, setPartial] = useState('');
-  const [loadedModel, setLoadedModel] = useState();
-  const [recognizer, setRecognizer] = useState();
-  const [loading, setLoading] = useState(false);
-  const [ready, setReady] = useState(false);
-
-  const loadModel = async path => {
-    setLoading(true);
-    loadedModel?.model.terminate();
-
-    const model = await createModel(`/vosk-models/${path}`);
-
-    setLoadedModel({ model, path });
-    const recognizer = new model.KaldiRecognizer(48000);
-    recognizer.setWords(true);
-    recognizer.on('result', message => {
-      const { result } = message;
-      setUtterances(utt => [...utt, result]);
-    });
-
-    recognizer.on('partialresult', message => {
-      setPartial(message.result.partial);
-    });
-
-    setRecognizer(() => {
-      setLoading(false);
-      setReady(true);
-      return recognizer;
-    });
-  };
+  const {
+    loadModel,
+    loadedModel,
+    ready,
+    setReady,
+    loading,
+    recognizer,
+    utterances,
+    partial,
+  } = useVosk();
 
   return (
     <Wrapper>
--- react-ui/src/hooks/useVosk.js
+++ react-ui/src/hooks/useVosk.js
@@ -1,3 +1,49 @@
-const useVosk = () => {};
+import { useState } from 'react';
+import { createModel } from 'vosk-browser';
+
+const useVosk = () => {
+  const [utterances, setUtterances] = useState([]);
+  const [partial, setPartial] = useState('');
+  const [loadedModel, setLoadedModel] = useState();
+  const [recognizer, setRecognizer] = useState();
+  const [loading, setLoading] = useState(false);
+  const [ready, setReady] = useState(false);
+
+  const loadModel = async path => {
+    setLoading(true);
+    loadedModel?.model.terminate();
+
+    const model = await createModel(`/vosk-models/${path}`);
+
+    setLoadedModel({ model, path });
+    const recognizer = new model.KaldiRecognizer(48000);
+    recognizer.setWords(true);
+    recognizer.on('result', message => {
+      const { result } = message;
+      setUtterances(utt => [...utt, result]);
+    });
+
+    recognizer.on('partialresult', message => {
+      setPartial(message.result.partial);
+    });
+
+    setRecognizer(() => {
+      setLoading(false);
+      setReady(true);
+      return recognizer;
+    });
+  };
+
+  return {
+    loadModel,
+    loadedModel,
+    ready,
+    setReady,
+    loading,
+    recognizer,
+    utterances,
+    partial,
+  };
+};
 
 export default useVosk;
