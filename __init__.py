4b2e161368a93fb79b76f8819e6ee9aa43c478dc
norgeous <daniel@harte.me>
2024-05-26 16:59:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,10 +6,10 @@ from aiohttp import web
 
 WEBROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "react-ui/dist")
 
-@server.PromptServer.instance.routes.get("/ui")
+@server.PromptServer.instance.routes.get("/ui/")
 def dungeon_entrance(request):
     return web.FileResponse(os.path.join(WEBROOT, "index.html"))
 
-server.PromptServer.instance.routes.static("/ui", path=WEBROOT)
+server.PromptServer.instance.routes.static("/ui/", path=WEBROOT)
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
