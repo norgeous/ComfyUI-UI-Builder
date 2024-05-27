68655665ba1baaa1b17463cfcf7635ac7f568870
norgeous <daniel@harte.me>
2024-05-27 17:44:27 +0100

    lint

--- react-ui/src/components/Accordion/AccordionGroup.jsx
+++ react-ui/src/components/Accordion/AccordionGroup.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { FaChevronUp } from 'react-icons/fa6';
 
