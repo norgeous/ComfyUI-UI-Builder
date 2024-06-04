acfe378748ddc5a409a990b57778170d7e96332c
norgeous <daniel@harte.me>
2024-06-04 23:15:33 +0100

    tidy

--- react-ui/src/components/globalStyles.js
+++ react-ui/src/components/globalStyles.js
@@ -9,46 +9,17 @@ export const Theme1Style = createGlobalStyle`
 `;
 
 export const GlobalStyle = createGlobalStyle`
-  /* :root {
-    --primary: oklch(50% var(--chroma1) var(--hue1));
-    --hue2: calc(var(--hue1) + 90);
-    
-    --bg-base: white;
-    --fg-base: black;
-
-    @media (prefers-color-scheme: dark) {
-      --bg-base: black;
-      --fg-base: white;
-    }
-
-    --page-bg: color-mix(in srgb, var(--bg-base) 95%, var(--primary));
-    --page-fg: color-mix(in srgb, var(--fg-base) 95%, var(--primary));
-
-    --surface-bg: color-mix(in srgb, var(--bg-base) 85%, var(--primary));
-    --surface-fg: color-mix(in srgb, var(--fg-base) 85%, var(--primary));
-
-    --header-bg: color-mix(in srgb, var(--bg-base) 75%, var(--primary));
-    --header-fg: color-mix(in srgb, var(--fg-base) 75%, var(--primary));
-
-    --input-outline: var(--primary);
-    --input-border: color-mix(in srgb, var(--bg-base) 50%, var(--primary));
-    --input-bg: color-mix(in srgb, var(--bg-base) 75%, var(--primary));
-    --input-fg: color-mix(in srgb, var(--fg-base) 50%, var(--primary));
-  } */
-
   :root {
+    --fg: #111;
     --lightness-bg1: 90%;
     --lightness-bg2: 85%;
     --lightness-bg3: 80%;
-    --lightness-fg: 8%;
-    --fg: #111;
 
     @media (prefers-color-scheme: dark) {
+      --fg: #eee;
       --lightness-bg1: 10%;
       --lightness-bg2: 15%;
       --lightness-bg3: 20%;
-      --lightness-fg: 92%;
-      --fg: #eee;
     }
 
     --primary: hsl(var(--hue1) var(--saturation1) 50%);  
@@ -59,7 +30,6 @@ export const GlobalStyle = createGlobalStyle`
     --surface-bg: hsl(var(--hue1) var(--saturation1) var(--lightness-bg2));
     --header-bg: hsl(var(--hue1) var(--saturation1) var(--lightness-bg3));
     --input-bg: hsl(var(--hue1) var(--saturation1) var(--lightness-bg3));
-    
   }
 
   body {
