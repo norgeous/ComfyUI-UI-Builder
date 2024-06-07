2c53651b6cc59451d017e15135bdf01b19074f7e
norgeous <daniel@harte.me>
2024-06-07 19:47:15 +0100

    Update InputSeed.jsx

--- react-ui/src/components/form-fields/InputSeed/InputSeed.jsx
+++ react-ui/src/components/form-fields/InputSeed/InputSeed.jsx
@@ -6,6 +6,22 @@ const InputSeed = ({ value, ...props }) => {
   return (
     <InputNumber {...props} value={seed}>
       i am seed
+      {/* "subComponents": [
+        {
+          "id": "enableSeedRandomisation",
+          "label": "Randomise",
+          "type": "checkbox",
+          "defaultValue": false
+        },
+        {
+          "id": "shuffle",
+          "label": "Shuffle",
+          "type": "shuffle",
+          "targetId": "seed",
+          "onChangeData": { "enableSeedRandomisation": false }
+        }
+      ],
+      "onChangeData": { "enableSeedRandomisation": false } */}
     </InputNumber>
   );
 };
