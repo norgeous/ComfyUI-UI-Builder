4f0b2f3f6e7177aef8f98f007271e0f0487b50d8
norgeous <daniel@harte.me>
2024-06-08 16:59:31 +0100

    Update InputSpeech.jsx

--- react-ui/src/components/form-fields/InputSpeech/InputSpeech.jsx
+++ react-ui/src/components/form-fields/InputSpeech/InputSpeech.jsx
@@ -27,14 +27,14 @@ const Textarea = styled.textarea`
   background: var(--input-bg);
   color: var(--input-fg);
   border-radius: var(--radius);
-  &:focus-visible {
-    outline: 2px solid var(--input-outline);
-    outline-offset: 2px;
-  }
   min-height: 80px;
   line-height: 21px;
   padding: 4px 8px;
   resize: vertical;
+  &:focus-visible {
+    outline: 2px solid var(--input-outline);
+    outline-offset: 2px;
+  }
 `;
 
 const InputTextarea = ({
