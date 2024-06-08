6e1880597ab4dc4ed2567f687abb5dec549a78ee
norgeous <daniel@harte.me>
2024-06-08 13:26:42 +0100

    Update SimpleStateDecorator.jsx

--- react-ui/.storybook/SimpleStateDecorator.jsx
+++ react-ui/.storybook/SimpleStateDecorator.jsx
@@ -1,12 +1,23 @@
 import { action } from '@storybook/addon-actions';
 import { useRef, useState } from 'react';
+import styled from 'styled-components';
+import { FaArrowRotateLeft } from 'react-icons/fa6';
 import InputRefContext from '@/contexts/InputRefContext';
-import Reset from '@/components/header-components/Reset';
+import HeaderButton from '@/components/header-components/HeaderButton';
+
+const ResetButton = styled(HeaderButton)`
+  margin-left: auto;
+`;
+
+const ResetIcon = styled(FaArrowRotateLeft)`
+  display: block;
+  font-size: 12px;
+`;
 
 const SimpleStateDecorator = (Story, { args }) => {
   const ref = useRef();
 
-  const { id, defaultValue, defaultValueIndex, options } = args;
+  const { defaultValue, defaultValueIndex, options } = args;
 
   const initialValue = defaultValue ?? options[defaultValueIndex].value;
 
@@ -24,7 +35,15 @@ const SimpleStateDecorator = (Story, { args }) => {
 
   const showReset = value !== initialValue;
 
-  const children = <>{showReset && <Reset onClick={handleReset} />}</>;
+  const children = (
+    <>
+      {showReset && (
+        <ResetButton label="Reset" onClick={handleReset}>
+          <ResetIcon />
+        </ResetButton>
+      )}
+    </>
+  );
 
   return (
     <InputRefContext.Provider value={ref}>
