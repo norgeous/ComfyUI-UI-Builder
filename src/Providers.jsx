3ad890951bc0275f4974f79f48f4f4ae8fb81e35
norgeous <daniel@harte.me>
2024-06-10 00:49:09 +0100

    Pyramid directory

--- src/Providers.jsx
+++ src/Providers.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
-import Pyramid from './components/Pyramid';
-import ClientIdProvider from './contexts/ClientIdProvider';
-import ConfigsProvider from './contexts/ConfigsProvider';
-import FormProvider from './contexts/FormProvider';
-import ObjectInfoProvider from './contexts/ObjectInfoProvider';
-import WsProvider from './contexts/WsProvider';
-import AppProvider from './contexts/AppProvider';
-import SpeechProvider from './contexts/SpeechProvider';
+import Pyramid from '@/components/Pyramid/Pyramid';
+import ClientIdProvider from '@/contexts/ClientIdProvider';
+import ConfigsProvider from '@/contexts/ConfigsProvider';
+import FormProvider from '@/contexts/FormProvider';
+import ObjectInfoProvider from '@/contexts/ObjectInfoProvider';
+import WsProvider from '@/contexts/WsProvider';
+import AppProvider from '@/contexts/AppProvider';
+import SpeechProvider from '@/contexts/SpeechProvider';
 
 const Providers = ({ children = null }) => (
   <Pyramid>
