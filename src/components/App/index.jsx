f76d46ee5d6ef0d553170318ecfa7a08a0e745d3
norgeous <daniel@harte.me>
2024-06-11 13:16:22 +0100

    Update index.jsx

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,24 +1,7 @@
-import styled from 'styled-components';
 import { Theme } from '@/components/globalStyles';
-import Sidebar from '@/components/Sidebar';
 import MainContent from '@/components/MainContent';
-
-const Page = styled.div`
-  display: grid;
-  height: 100svh;
-
-  // portrait first
-  grid-template-rows: minmax(50%, auto) minmax(auto, 400px);
-
-  // landscape overrides
-  @media (min-aspect-ratio: 1/1) {
-    grid-template-rows: none;
-    grid-template-columns: minmax(auto, 460px) minmax(50%, auto);
-    & > :first-child {
-      order: 2; // change the order on landscape, so that sidebar is first
-    }
-  }
-`;
+import Sidebar from '@/components/Sidebar';
+import { Page } from './styled';
 
 const App = () => (
   <>
