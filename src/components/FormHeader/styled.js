e6cf92578230e963ecc0708a403529e9ca389460
Daniel Harte <norgeous@users.noreply.github.com>
2024-06-11 22:20:39 +0100

    Update styled.js

--- src/components/FormHeader/styled.js
+++ src/components/FormHeader/styled.js
@@ -4,7 +4,7 @@ export const Header = styled.header`
   background: var(--surface-bg);
 `;
 
-export const Description = styled.header`
+export const Description = styled.div`
   padding: 0 8px 8px;
   font-size: 12px;
 `;
