d39e995b007c8f95a34d52e01ff706004365b4e5
norgeous <daniel@harte.me>
2024-06-11 14:53:14 +0100

    chop

--- src/components/HeaderButton/index.jsx
+++ src/components/HeaderButton/index.jsx
@@ -1,27 +1,6 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import Tooltip from '@/components/Tooltip';
-
-const InputHeaderButton = styled.button`
-  cursor: pointer;
-  display: block;
-  border: none;
-  color: inherit;
-  background: transparent;
-  font-size: 0.75rem; // 12px
-  padding: 0;
-  &:focus-visible {
-    outline: 2px solid var(--input-outline);
-    outline-offset: 2px;
-    border-radius: var(--radius);
-  }
-  > * {
-    opacity: 0.5;
-  }
-  &:hover > * {
-    opacity: 1;
-  }
-`;
+import { InputHeaderButton } from './styled';
 
 const HeaderButton = ({
   className = undefined,
