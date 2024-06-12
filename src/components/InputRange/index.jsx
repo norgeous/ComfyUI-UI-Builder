f171b34999200d473fad0da44e40079f4faf8bcd
norgeous <daniel@harte.me>
2024-06-12 15:12:21 +0100

    chop styled.js for Range

--- src/components/InputRange/index.jsx
+++ src/components/InputRange/index.jsx
@@ -1,101 +1,18 @@
 import { useContext } from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
 import InputRefContext from '@/contexts/InputRefContext';
 import deepEqual from '@/utils/deepEqual';
 import { ArrowLeftIcon, ArrowRightIcon } from '@/components/Icons';
 import InputWrapper from '@/components/InputWrapper';
 import InputHeader from '@/components/InputHeader';
-
-const RangeWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 3.33333333px; /* to make it the same height as a text input */
-`;
-
-const track = css`
-  width: 100%;
-  height: 8px;
-  cursor: pointer;
-  background: var(--input-bg);
-  border: 1px solid var(--input-outline);
-  border-radius: 100px;
-`;
-
-const thumb = css`
-  border: none;
-  border: 2px solid #fff;
-  box-sizing: border-box;
-  height: 20px;
-  width: 20px;
-  border-radius: 10px;
-  background: var(--input-outline);
-  cursor: pointer;
-`;
-
-const progress = css`
-  background: var(--input-outline);
-  height: 8px;
-`;
-
-const Input = styled.input.attrs({ type: 'range' })`
-  display: block;
-  width: 100%;
-  height: 20px;
-  min-width: 100%;
-  box-sizing: border-box;
-  accent-color: var(--input-outline);
-  margin: 0;
-  background: transparent;
-  &::-moz-range-track {
-    ${track}
-  }
-  &::-moz-range-progress {
-    ${progress}
-  }
-  &::-moz-range-thumb {
-    ${thumb}
-  }
-
-  -webkit-appearance: none;
-  &::-webkit-slider-runnable-track {
-    ${track}
-  }
-  &::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    margin-top: -6px;
-    ${thumb}
-  }
-`;
-
-const Sublabels = styled.div`
-  padding: ${({ $isPips }) => ($isPips ? '0 10px' : 0)};
-  display: flex;
-  justify-content: space-between;
-  font-size: 12px;
-`;
-
-const Sublabel = styled.div`
-  cursor: pointer;
-  display: inline-flex;
-  place-items: center;
-  gap: 4px;
-  overflow: hidden;
-`;
-
-const SublabelText = styled.div`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
-
-const Pip = styled.div`
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  cursor: pointer;
-  width: 0;
-`;
+import {
+  RangeWrapper,
+  Input,
+  Sublabels,
+  Sublabel,
+  SublabelText,
+  Pip,
+} from './styled';
 
 const InputRange = ({
   id = undefined,
