f6050e6f263a24fb9e7415500542542b16ee9f8e
norgeous <daniel@harte.me>
2024-06-12 15:08:35 +0100

    :knife: chop Btn styled.js

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,31 +1,5 @@
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
-
-const Btn = styled.button.attrs({ type: 'button' })`
-  background: var(--input-outline);
-  margin: 0;
-  border: none;
-  ${({ $wide }) =>
-    $wide &&
-    css`
-      width: 100%;
-    `}
-  border-radius: var(--radius);
-  padding: 8px;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  gap: 4px;
-  ${({ $variant }) => {
-    if ($variant === 'secondary')
-      return css`
-        background: red;
-      `;
-    return undefined;
-  }}
-  font-size: 14px;
-  line-height: 14px;
-`;
+import { Btn } from './styled';
 
 const Button = ({
   wide = false,
