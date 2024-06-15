20903b8ef5b6f8ba10f1f5045a943221b91ab47a
norgeous <daniel@harte.me>
2024-06-16 00:41:14 +0100

    Update index.stories.js

--- src/components/App/index.stories.js
+++ src/components/App/index.stories.js
@@ -9,23 +9,23 @@ export const App = {
   parameters: {
     layout: 'fullscreen',
     screenshot: {
+      viewport: {
+        width: 830,
+        height: 600,
+      },
       variants: {
         small: {
           viewport: 'iPhone 5',
         },
-        large: {
-          viewport: {
-            width: 830,
-            height: 600,
-          },
-        },
-        large_dark: {
-          viewport: {
-            width: 830,
-            height: 600,
-          },
-          forceDark: true,
-        },
+        // large: {
+        //   viewport: {
+        //     width: 830,
+        //     height: 600,
+        //   },
+        // },
+        // large_dark: {
+        //   forceDark: true,
+        // },
       },
     },
   },
