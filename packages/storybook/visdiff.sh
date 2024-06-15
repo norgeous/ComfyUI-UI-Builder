c59b7a5f721eb8987914718d06251098a60d527e
Daniel Harte <norgeous@users.noreply.github.com>
2024-06-15 20:35:05 +0100

    Update visdiff.sh

--- packages/storybook/visdiff.sh
+++ packages/storybook/visdiff.sh
@@ -4,11 +4,11 @@ cd ../../
 rm -r ./packages/storybook/visdiff-report/ # delete the old report
 mkdir ./packages/storybook/visdiff-report/
 
-git checkout main _storycaps_ # checkout the _storycaps_ folder from main to current branch
+# git checkout main _storycaps_ # checkout the _storycaps_ folder from main to current branch
 cp -r ./_storycaps_ ./packages/storybook/visdiff-report/expected/ # copy main's baseline in the report's /expected/ folder
 
 rm -r ./_storycaps_/ # remove the _storycaps_ currently on branch, as a story might have been removed
-npm run storycap -w packages/storybook # generate a new _storycaps_ baseline
 
+npm run storycap -w packages/storybook # generate a new _storycaps_ baseline
 npm run regression -w packages/storybook # generate the report
 npm run serve:visdiff -w packages/storybook # serve the report
