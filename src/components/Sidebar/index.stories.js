2e7c2df25cf7f812f49816db91340dd010f82516
norgeous <daniel@harte.me>
2024-06-16 17:39:28 +0100

    Update index.stories.js

--- src/components/Sidebar/index.stories.js
+++ src/components/Sidebar/index.stories.js
@@ -8,6 +8,12 @@ export default {
 export const Sidebar = {
   parameters: {
     layout: 'fullscreen',
+    screenshot: {
+      viewport: {
+        width: 413, // half the width of github README.md on desktop
+        height: 700,
+      },
+    },
   },
   args: {},
 };
