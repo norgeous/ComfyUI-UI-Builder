a4b053fe9732dd7e8b4b7524435f6904606c5784
Daniel Harte <norgeous@users.noreply.github.com>
2024-06-19 17:36:29 +0100

    repair theming vite

--- src/components/Pyramid/index.js
+++ src/components/Pyramid/index.js
@@ -2,9 +2,9 @@ import { cloneElement } from 'react';
 
 // stack children into a pyramid of doom
 const Pyramid = ({ children = null }) =>
-  children?.reduceRight(
-    (acc, currentValue) => cloneElement(currentValue, {}, acc),
-    null,
-  );
+  children?.reduceRight((acc, currentValue) => {
+    if (Array.isArray(currentValue)) return currentValue;
+    return cloneElement(currentValue, {}, acc);
+  }, null);
 
 export default Pyramid;
