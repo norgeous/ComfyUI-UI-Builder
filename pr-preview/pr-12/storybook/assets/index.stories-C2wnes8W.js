6c487365dc9c8b435cb03e3172313fbb8993d203
Daniel Harte <norgeous@users.noreply.github.com>
2024-06-20 15:42:08 +0100

    DEL pr12

--- pr-preview/pr-12/storybook/assets/index.stories-C2wnes8W.js
+++
@@ -1,9 +0,0 @@
-import{S as r}from"./SimpleStateDecorator-D2SI5kQ0.js";import{c as e}from"./index-DgClLmqX.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-CqhhQUir.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-Csld0TKj.js";import"./index-D3ylJrlI.js";import"./index-CCoYZxa1.js";import"./index-DheTTISF.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-DizZy_GM.js";import"./index-CaMEndn5.js";import"./index-DCccdXUQ.js";import"./index-x7YGS4Zy.js";const D={title:"InputText",component:e,decorators:[r]},t={args:{id:"demo",label:"My InputText Label",info:"info text",options:["Option 1","Option 2","Option 3","Option 4","Option 5","Option 6"],defaultValue:""}};var o,p,i;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    id: 'demo',
-    label: 'My InputText Label',
-    info: 'info text',
-    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6'],
-    defaultValue: ''
-  }
-}`,...(i=(p=t.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const E=["InputText"];export{t as InputText,E as __namedExportsOrder,D as default};
