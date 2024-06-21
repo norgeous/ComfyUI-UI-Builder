1c68ba1ca57131e91ac3eed1683e6415701c2007
norgeous <norgeous@users.noreply.github.com>
2024-06-21 15:00:46 +0000

    Deploy preview for PR 16 🛫

---
+++ pr-preview/pr-16/storybook/assets/index.stories-CvHl3G3z.js
@@ -0,0 +1,8 @@
+import{S as p}from"./SimpleStateDecorator-D2SI5kQ0.js";import{c as m}from"./index-C-iZoxKA.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-CqhhQUir.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-Csld0TKj.js";import"./index-D3ylJrlI.js";import"./index-CCoYZxa1.js";import"./index-DheTTISF.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-DizZy_GM.js";import"./ObjectInfoContext-CYsF4dpo.js";import"./index-DgClLmqX.js";import"./index-CaMEndn5.js";import"./index-DCccdXUQ.js";import"./index-x7YGS4Zy.js";const E={title:"InputSelectCkpt",component:m,decorators:[p]},t={args:{id:"demo",label:"My InputSelectCkpt Label",info:"help text",defaultValue:""}};var o,r,e;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputSelectCkpt Label',
+    info: 'help text',
+    defaultValue: ''
+  }
+}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const O=["InputSelectCkpt"];export{t as InputSelectCkpt,O as __namedExportsOrder,E as default};
---
+++ pr-preview/pr-16/storybook/assets/index.stories-UUWawx1P.js
@@ -0,0 +1,3 @@
+import{P as s}from"./index-gjkr9lMi.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";const d={title:"Pyramid",component:s},r={args:{}};var a,e,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const c=["Pyramid"];export{r as Pyramid,c as __namedExportsOrder,d as default};
