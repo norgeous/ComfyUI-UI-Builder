2f98e8499c5c19e63993eca34fc3c42ee6ffba5a
Daniel Harte <norgeous@users.noreply.github.com>
2024-06-21 15:21:03 +0100

    Delete pr-preview/pr-13 directory

--- pr-preview/pr-13/storybook/assets/index.stories-C6cCv7vA.js
+++
@@ -1,3 +0,0 @@
-import{c as e}from"./index-WIqSUmum.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./WsContext-9iT9ZgtG.js";const p={title:"StatusLight",component:e},t={args:{}};var r,s,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {}
-}`,...(o=(s=t.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const n=["StatusLight"];export{t as StatusLight,n as __namedExportsOrder,p as default};
