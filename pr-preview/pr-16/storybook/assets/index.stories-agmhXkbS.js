6c4dbf4a801c98620a1460be2b4fe5d1d4139baa
norgeous <norgeous@users.noreply.github.com>
2024-06-21 15:28:00 +0000

    Remove preview for PR 16 🛬

--- pr-preview/pr-16/storybook/assets/index.stories-agmhXkbS.js
+++
@@ -1,8 +0,0 @@
-import{c as n}from"./index-DCccdXUQ.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-x7YGS4Zy.js";import"./index-DizZy_GM.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BAMY2Nnw.js";import"./index-DheTTISF.js";import"./index-BOkhicXD.js";const H={title:"InputHeader",component:n},e={args:{id:"test",label:"My InputHeader Label",info:"my info text",children:"extra children"}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    id: 'test',
-    label: 'My InputHeader Label',
-    info: 'my info text',
-    children: 'extra children'
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const I=["InputHeader"];export{e as InputHeader,I as __namedExportsOrder,H as default};
