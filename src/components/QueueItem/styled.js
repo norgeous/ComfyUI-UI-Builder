3628cb9ddbe322f4eaa055dbc78c7c6ea641ea0f
norgeous <daniel@harte.me>
2024-06-22 22:55:34 +0100

    Update styled.js

--- src/components/QueueItem/styled.js
+++ src/components/QueueItem/styled.js
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 // eslint-disable-next-line import/prefer-default-export
 export const QueueTitle = styled.div`
   flex-grow: 1;
+  font-size: 10px;
+  white-space: nowrap;
 `;
