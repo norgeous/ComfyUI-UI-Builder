1f7006e8197549571690e9eed557e3ab66ae6c88
norgeous <daniel@harte.me>
2024-06-22 23:13:13 +0100

    Update styled.js

--- src/components/Progress/styled.js
+++ src/components/Progress/styled.js
@@ -5,13 +5,14 @@ export const Progress = styled.progress.attrs({ max: 1 })`
   appearance: none;
   display: block;
   width: 100%;
-  border-radius: 0;
   height: 8px;
   border: 0;
+  min-width: 20px;
 
   // webkit (chrome)
   &::-webkit-progress-bar {
     background: var(--bg2);
+    border-radius: var(--radius);
   }
   &::-webkit-progress-value {
     background: var(--accent);
@@ -19,6 +20,7 @@ export const Progress = styled.progress.attrs({ max: 1 })`
 
   // firefox
   background: var(--bg2); // firefox only
+  border-radius: var(--radius); // firefox only
   &::-moz-progress-bar {
     background: var(--accent);
   }
