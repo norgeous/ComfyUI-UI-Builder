b0fe171853deec4dbfa6c6a11551fadae6703643
norgeous <daniel@harte.me>
2024-06-22 22:37:32 +0100

    move progress

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,19 +1,17 @@
 import { useContext } from 'react';
 import WsContext from '@/contexts/WsContext';
-import Progress from '@/components/Progress';
 import ImageGrid from '@/components/ImageGrid';
 import StatusBar from '@/components/StatusBar';
 import { ContentArea, Main } from './styled';
 
 const MainContent = () => {
-  const { comfyUrl, progress, output } = useContext(WsContext);
+  const { comfyUrl, output } = useContext(WsContext);
   const images = (output?.images || []).map(
     ({ filename }) => `${comfyUrl}/view?type=output&filename=${filename}`,
   );
 
   return (
     <Main>
-      <Progress value={progress} />
       <ContentArea>
         <ImageGrid images={images} />
       </ContentArea>
--- src/components/QueueItem/index.jsx
+++ src/components/QueueItem/index.jsx
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Layout from '@/components/Layout';
 import Button from '@/components/Button';
 import { SpinnerIcon, InterruptIcon, WarningIcon } from '@/components/Icons';
 import Tooltip from '@/components/Tooltip';
+import Progress from '@/components/Progress';
+import WsContext from '@/contexts/WsContext';
 import { QueueTitle } from './styled';
 
 const getInterruptIcon = ({ interruptLoading, interruptError }) => {
@@ -18,23 +21,29 @@ const QueueItem = ({
   onInterrupt = () => {},
   interruptLoading = false,
   interruptError = '',
-}) => (
-  <Layout center>
-    {isLoading && <SpinnerIcon />}
-    <QueueTitle>
-      {jobName} ({status})
-    </QueueTitle>
-    <Tooltip text={interruptError || `Interrupt ${jobName}`}>
-      <Button
-        aria-label={interruptError || `Interrupt ${jobName}`}
-        disabled={interruptLoading}
-        onClick={onInterrupt}
-      >
-        {getInterruptIcon({ interruptLoading, interruptError })}
-      </Button>
-    </Tooltip>
-  </Layout>
-);
+}) => {
+  const { progress } = useContext(WsContext);
+
+  return (
+    <Layout center>
+      {isLoading && <SpinnerIcon />}
+      <QueueTitle>
+        {jobName} ({status})
+      </QueueTitle>
+
+      <Progress value={progress} />
+      <Tooltip text={interruptError || `Interrupt ${jobName}`}>
+        <Button
+          aria-label={interruptError || `Interrupt ${jobName}`}
+          disabled={interruptLoading}
+          onClick={onInterrupt}
+        >
+          {getInterruptIcon({ interruptLoading, interruptError })}
+        </Button>
+      </Tooltip>
+    </Layout>
+  );
+};
 
 QueueItem.propTypes = {
   jobName: PropTypes.string,
