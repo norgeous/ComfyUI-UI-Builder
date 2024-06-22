7ae243f681bcfdb3d5395304ad9393d617c33018
norgeous <norgeous@users.noreply.github.com>
2024-06-22 23:46:41 +0000

    Remove preview for PR 21 🛬

--- pr-preview/pr-21/storybook/assets/index.stories-BB93Q5V7.js
+++
@@ -1,5 +0,0 @@
-import{f as m}from"./index-Ct7ZYFme.js";import{c as i}from"./index-BMNAc7p0.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-D_SX36Yj.js";import"./index-8yc3hoqd.js";import"./floating-ui.react-CDqbCaSC.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-DizZy_GM.js";const E={title:"ResetButton",component:i},t={args:{onClick:m()}};var o,r,e;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    onClick: fn()
-  }
-}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const O=["ResetButton"];export{t as ResetButton,O as __namedExportsOrder,E as default};
--- pr-preview/pr-21/storybook/assets/index.stories-ZbOT2lyZ.js
+++
@@ -1,9 +0,0 @@
-import{S as r}from"./SimpleStateDecorator-DgRwP0Ki.js";import{c as e}from"./index-p7gvV1Lk.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Ct7ZYFme.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-BMNAc7p0.js";import"./index-D3ylJrlI.js";import"./index-D_SX36Yj.js";import"./index-8yc3hoqd.js";import"./floating-ui.react-CDqbCaSC.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-DizZy_GM.js";import"./index-CaMEndn5.js";import"./index-BXXglQ6L.js";import"./index-DAZpHX12.js";const E={title:"InputText",component:e,decorators:[r]},t={args:{id:"demo",label:"My InputText Label",info:"info text",options:["Option 1","Option 2","Option 3","Option 4","Option 5","Option 6"],defaultValue:""}};var o,p,i;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    id: 'demo',
-    label: 'My InputText Label',
-    info: 'info text',
-    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6'],
-    defaultValue: ''
-  }
-}`,...(i=(p=t.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const h=["InputText"];export{t as InputText,h as __namedExportsOrder,E as default};
