da73f0fc889a94b91b87fafbe38cfb98da1631bf
norgeous <norgeous@users.noreply.github.com>
2024-06-22 13:38:10 +0000

    Remove preview for PR 19 🛬

--- pr-preview/pr-19/storybook/assets/index.stories-DEd3YoBY.js
+++
@@ -1,6 +0,0 @@
-import{c as a}from"./index-K3mJEBu3.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-DheTTISF.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BAMY2Nnw.js";const b={title:"HeaderButton",component:a},r={args:{label:"test label",children:"R"}};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    label: 'test label',
-    children: 'R'
-  }
-}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const g=["HeaderButton"];export{r as HeaderButton,g as __namedExportsOrder,b as default};
--- pr-preview/pr-19/storybook/assets/index.stories-HjahFe3s.js
+++
@@ -1,3 +0,0 @@
-import{c as s}from"./index-x7YGS4Zy.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-DizZy_GM.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BAMY2Nnw.js";import"./index-DheTTISF.js";import"./index-BOkhicXD.js";const l={title:"HeaderInfo",component:s},r={args:{}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {}
-}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const x=["HeaderInfo"];export{r as HeaderInfo,x as __namedExportsOrder,l as default};
