9c66aa0c15fcf09750a37404f9e74f0e07d5058b
norgeous <norgeous@users.noreply.github.com>
2024-06-23 12:56:44 +0000

    Remove preview for PR 23 🛬

--- pr-preview/pr-23/storybook/assets/index-BBRnhhpC.js
+++
@@ -1,3 +0,0 @@
-import{j as s}from"./jsx-runtime-X2b_N9AH.js";import{r}from"./index-uCp2LrAq.js";import{W as n}from"./WsContext-9iT9ZgtG.js";import{p as e}from"./styled-components.browser.esm-Drpn4GzW.js";const i=e.span`
-  color: white;
-`,m={DEFAULT:"⚫",CONNECTING:"🔵",CONNECTED:"🟢",DISCONNECTED:"🔴"},t=()=>{const{wsStatus:o}=r.useContext(n);return s.jsx(i,{children:m[o]})},E=t;t.__docgenInfo={description:"",methods:[],displayName:"StatusLight"};export{E as c};
