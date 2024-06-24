36a367ccd0a192163fd96723665aa74fd85825f2
norgeous <norgeous@users.noreply.github.com>
2024-06-24 08:27:25 +0000

    Remove preview for PR 24 🛬

--- pr-preview/pr-24/storybook/assets/index.stories-C7ACfZmL.js
+++
@@ -1,8 +0,0 @@
-import{S as p}from"./SimpleStateDecorator-Bi8hSyFQ.js";import{c as m}from"./index-BUg0OLFU.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Ct7ZYFme.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-gNuICqym.js";import"./index-D3ylJrlI.js";import"./index-D_SX36Yj.js";import"./index-8yc3hoqd.js";import"./floating-ui.react-CDqbCaSC.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BiKVFdKX.js";import"./ObjectInfoContext-CYsF4dpo.js";import"./index-JzUIFCbj.js";import"./index-qDzr2xAS.js";import"./index-BZF6B-3v.js";import"./index-DaNusNhd.js";const O={title:"InputSelectCkpt",component:m,decorators:[p]},t={args:{id:"demo",label:"My InputSelectCkpt Label",info:"help text",defaultValue:""}};var o,r,e;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    id: 'demo',
-    label: 'My InputSelectCkpt Label',
-    info: 'help text',
-    defaultValue: ''
-  }
-}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const j=["InputSelectCkpt"];export{t as InputSelectCkpt,j as __namedExportsOrder,O as default};
