6fa5578079a8c950df4d3dfd7e290b9ef2c529f0
norgeous <norgeous@users.noreply.github.com>
2024-06-24 08:02:16 +0000

    Deploy preview for PR 24 🛫

---
+++ pr-preview/pr-24/storybook/assets/experimental.stories-QBqLlqzp.js
@@ -0,0 +1,40 @@
+import{j as n}from"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";const x={title:"_Experiments/OKLCH"},t={parameters:{screenshot:{skip:!0}},args:{saturationOrChroma:100,leftHalf:!1},render:({saturationOrChroma:s,leftHalf:a})=>{const m=a?50:100,o=a?1:2;return n.jsxs(n.Fragment,{children:[n.jsxs("h1",{children:["hsl",a&&" (showing left half only)"]}),Array.from({length:360/20+1},(r,e)=>e*20).map(r=>n.jsx("div",{children:Array.from({length:m/o+1},(e,l)=>l*o).map(e=>n.jsx("input",{type:"checkbox",checked:!0,style:{accentColor:`hsl(${r}, ${s}%, ${e}%)`},title:`L:${e} H:${r}`,"aria-label":"checkbox"},`${e}-${r}`))},r)),n.jsx("h1",{children:"oklch"}),Array.from({length:360/20+1},(r,e)=>e*20).map(r=>n.jsx("div",{children:Array.from({length:100/2+1},(e,l)=>l*2).map(e=>n.jsx("input",{type:"checkbox",checked:!0,style:{accentColor:`oklch(${e}% ${s}% ${r})`},title:`L:${e} H:${r}`,"aria-label":"checkbox"},`${e}-${r}`))},r))]})}};var c,h,i;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  parameters: {
+    screenshot: {
+      skip: true
+    }
+  },
+  args: {
+    saturationOrChroma: 100,
+    leftHalf: false
+  },
+  render: ({
+    saturationOrChroma,
+    leftHalf
+  }) => {
+    const maxL = leftHalf ? 50 : 100;
+    const step = leftHalf ? 1 : 2;
+    return <>
+        <h1>hsl{leftHalf && \` (showing left half only)\`}</h1>
+        {Array.from({
+        length: 360 / 20 + 1
+      }, (_, i) => i * 20).map(h => <div key={h}>
+            {Array.from({
+          length: maxL / step + 1
+        }, (_, i) => i * step).map(l => <input key={\`\${l}-\${h}\`} type="checkbox" checked style={{
+          accentColor: \`hsl(\${h}, \${saturationOrChroma}%, \${l}%)\`
+        }} title={\`L:\${l} H:\${h}\`} aria-label="checkbox" />)}
+          </div>)}
+        <h1>oklch</h1>
+        {Array.from({
+        length: 360 / 20 + 1
+      }, (_, i) => i * 20).map(h => <div key={h}>
+            {Array.from({
+          length: 100 / 2 + 1
+        }, (_, i) => i * 2).map(l => <input key={\`\${l}-\${h}\`} type="checkbox" checked style={{
+          accentColor: \`oklch(\${l}% \${saturationOrChroma}% \${h})\`
+        }} title={\`L:\${l} H:\${h}\`} aria-label="checkbox" />)}
+          </div>)}
+      </>;
+  }
+}`,...(i=(h=t.parameters)==null?void 0:h.docs)==null?void 0:i.source}}};const d=["HSLVsLCH"];export{t as HSLVsLCH,d as __namedExportsOrder,x as default};
---
+++ pr-preview/pr-24/storybook/assets/index-BH_a3JuL.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-X2b_N9AH.js";import{c as d}from"./index-arn3qWY0.js";import{c as u}from"./index-rCZd8bAW.js";import{c as x}from"./index-CUNpqF_p.js";import{c as l}from"./index-qDzr2xAS.js";import{r as e}from"./index-uCp2LrAq.js";import{A as f}from"./AppContext-DX1Wlwzv.js";import{W as g}from"./WsContext-9iT9ZgtG.js";import{c as j}from"./index-BVWtDH-0.js";import{p as n}from"./styled-components.browser.esm-Drpn4GzW.js";const s=()=>{const{progress:t,lastWsMessage:p,isGenerating:r}=e.useContext(g),{executeInterrupt:a,interruptLoading:c,interruptError:m}=e.useContext(f);return r?o.jsx(j,{isLoading:r,status:`${p} ${r&&`${Math.round(t*100)}%`}`,progress:t,onInterrupt:a,interruptLoading:c,interruptError:m}):null};s.__docgenInfo={description:"",methods:[],displayName:"Queue"};const h=n.aside`
+  display: flex;
+  flex-direction: column;
+
+  background: var(--bg1);
+  color: var(--fg1);
+
+  overflow: hidden;
+  height: 100%;
+`,$=n.div`
+  padding: 8px;
+  overflow-y: auto;
+  flex-grow: 1;
+`,i=()=>o.jsxs(h,{children:[o.jsx(d,{}),o.jsx($,{children:o.jsx(u,{})}),o.jsxs(l,{pad:!0,column:!0,children:[o.jsx(s,{}),o.jsx(x,{})]})]}),A=i;i.__docgenInfo={description:"",methods:[],displayName:"Sidebar"};export{A as c};
