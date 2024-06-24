76ab0d9b5298d32e6903f61ddf3062e203aae263
norgeous <norgeous@users.noreply.github.com>
2024-06-24 17:02:25 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-dj6VjCRh.js
@@ -0,0 +1,8 @@
+import{S as a}from"./SimpleStateDecorator-CZxMnIPa.js";import{c as p}from"./index-D9ccz6_9.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Ct7ZYFme.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-DoI6-4dv.js";import"./index-D3ylJrlI.js";import"./index-BwXAjfjY.js";import"./index-Coctz2Fv.js";import"./floating-ui.react-J0QxEdvZ.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BiKVFdKX.js";import"./index-qDzr2xAS.js";import"./index-B-jMyJAp.js";import"./index-Dk6XdMKP.js";const O={title:"InputTextarea",component:p,decorators:[a]},t={args:{id:"demo",label:"My InputTextarea Label",info:"info text",defaultValue:""}};var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputTextarea Label',
+    info: 'info text',
+    defaultValue: ''
+  }
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const h=["InputTextarea"];export{t as InputTextarea,h as __namedExportsOrder,O as default};
