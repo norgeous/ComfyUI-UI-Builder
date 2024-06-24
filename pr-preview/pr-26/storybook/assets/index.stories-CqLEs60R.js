4c8fc1da1dbc032e6e1c61cdc9c51e21cc4f1db7
norgeous <norgeous@users.noreply.github.com>
2024-06-24 21:37:00 +0000

    Deploy preview for PR 26 🛫

---
+++ pr-preview/pr-26/storybook/assets/index.stories-CqLEs60R.js
@@ -0,0 +1,10 @@
+import{S as e}from"./SimpleStateDecorator-BK54pHJ0.js";import{c as i}from"./index-C4Mw2SUm.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Ct7ZYFme.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-CSoN9Ezd.js";import"./index-D3ylJrlI.js";import"./index-DnpxmPVc.js";import"./index-DzcYuhaG.js";import"./floating-ui.react-J0QxEdvZ.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-CZ727q7L.js";import"./index-BiKVFdKX.js";import"./index-qDzr2xAS.js";import"./index-PlMGIOlZ.js";import"./index-OhTq4FGT.js";const h={title:"InputNumber",component:i,decorators:[e]},r={args:{id:"demo",label:"My InputNumber Label",info:"info text",defaultValue:69,min:42,max:420}};var t,o,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputNumber Label',
+    info: 'info text',
+    defaultValue: 69,
+    min: 42,
+    max: 420
+  }
+}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const j=["InputNumber"];export{r as InputNumber,j as __namedExportsOrder,h as default};
