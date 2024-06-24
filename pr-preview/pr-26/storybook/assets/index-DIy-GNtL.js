fbff967519e7361b7877c921318770626b56d405
norgeous <norgeous@users.noreply.github.com>
2024-06-24 18:31:43 +0000

    Deploy preview for PR 26 🛫

---
+++ pr-preview/pr-26/storybook/assets/index-DIy-GNtL.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{r as s}from"./index-uCp2LrAq.js";import{P as n}from"./index-D3ylJrlI.js";import{I as C}from"./InputRefContext-CmPhBSwH.js";import{S as q}from"./SpeechContext-cKuR6Onv.js";import{c as T}from"./index-qDzr2xAS.js";import{c as V}from"./index-B-jMyJAp.js";import{c as M}from"./index-BZDIBKFQ.js";import{M as d,S,j as $}from"./index-BiKVFdKX.js";import{c as u}from"./index-BwXAjfjY.js";import{p as k}from"./styled-components.browser.esm-Drpn4GzW.js";import{b as E}from"./index-BXd8L118.js";const U=k.textarea`
+  ${E(3)}
+  border: 1px solid var(--bg4);
+
+  display: block;
+  box-sizing: border-box;
+  width: 100%;
+  border-radius: var(--radius);
+  min-height: 60px;
+  line-height: 21px;
+  padding: 4px 8px;
+  resize: vertical;
+  &:focus-visible {
+    outline: 2px solid var(--accent);
+    outline-offset: 2px;
+  }
+`,l=({id:t=void 0,label:x=void 0,info:g=void 0,value:v=void 0,onChange:p=()=>{},children:h=null})=>{const b=s.useContext(C),{targetId:j,unmutedId:y,setUnmutedId:c,loading:a,error:m,vosk:o,tail:f}=s.useContext(q),r=j===t,i=y===t;return s.useEffect(()=>{r&&p(f)},[f]),e.jsxs(T,{pad:!0,column:!0,gap:"sm",children:[e.jsxs(V,{id:t,label:x,info:g,children:[!o&&!a&&e.jsx(u,{label:"Enable Speech Capture",onClick:()=>c(t),children:e.jsx(d,{})}),!o&&a&&r&&e.jsx(S,{}),!o&&a&&!r&&e.jsx(u,{label:"loading elsewhere",children:e.jsx(d,{})}),o&&e.jsx(u,{label:i?"Unmuted":"Muted",onClick:()=>c(i?void 0:t),children:i?e.jsx(d,{}):e.jsx($,{})}),h]}),m&&r&&e.jsx(M,{children:m}),e.jsx(U,{ref:b,id:t,value:v,onChange:I=>p(I.target.value)})]})};l.propTypes={id:n.string,label:n.string,info:n.string,onChange:n.func,value:n.any,children:n.node};const J=l;l.__docgenInfo={description:"",methods:[],displayName:"InputTextarea",props:{id:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},label:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},info:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},value:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"any"},required:!1},onChange:{defaultValue:{value:"() => {}",computed:!1},description:"",type:{name:"func"},required:!1},children:{defaultValue:{value:"null",computed:!1},description:"",type:{name:"node"},required:!1}}};export{J as c};
