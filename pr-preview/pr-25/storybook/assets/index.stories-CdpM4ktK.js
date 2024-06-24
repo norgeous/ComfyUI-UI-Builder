b477f25c8889fb501840cc0ac0165d7783c91af7
norgeous <norgeous@users.noreply.github.com>
2024-06-24 11:56:57 +0000

    Deploy preview for PR 25 🛫

---
+++ pr-preview/pr-25/storybook/assets/index.stories-CdpM4ktK.js
@@ -0,0 +1,11 @@
+import{u as i}from"./index-Ct7ZYFme.js";import{c as a}from"./index-ENyULWse.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./floating-ui.react-CDqbCaSC.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";const f={title:"Tooltip",component:a},t={args:{text:"This is the tooltip text!",children:"Hover Me"},play:async({canvasElement:s})=>{await i.hover(s.querySelector("span"))}};var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    text: 'This is the tooltip text!',
+    children: 'Hover Me'
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    await userEvent.hover(canvasElement.querySelector('span'));
+  }
+}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const g=["Tooltip"];export{t as Tooltip,g as __namedExportsOrder,f as default};
