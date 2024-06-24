6e415286b8fcfa765f1142edc6c4e77b9035f44e
norgeous <norgeous@users.noreply.github.com>
2024-06-24 08:13:28 +0000

    Deploy preview for PR 24 🛫

---
+++ pr-preview/pr-24/storybook/assets/index.stories-CkL2bY6s.js
@@ -0,0 +1,13 @@
+import{c as p}from"./index-BrpCIrR3.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-arn3qWY0.js";import"./ConfigsContext-JqLhkqbA.js";import"./index-CPXDHPf0.js";import"./index-D3ylJrlI.js";import"./InputRefContext-CmPhBSwH.js";import"./deepEqual-_sTZaZ0q.js";import"./index-BZF6B-3v.js";import"./index-DaNusNhd.js";import"./index-BiKVFdKX.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BAMY2Nnw.js";import"./index-8yc3hoqd.js";import"./floating-ui.react-CDqbCaSC.js";import"./index-BOkhicXD.js";import"./index-CaMEndn5.js";import"./index-mHXvSy_Q.js";import"./FormContext-7paO4gYw.js";import"./index-qDzr2xAS.js";import"./index-gNuICqym.js";import"./index-D_SX36Yj.js";import"./index-PH288ROH.js";import"./index-CySY1AEZ.js";import"./index-B8h7WkE-.js";import"./index-CXNmq0S4.js";import"./index-CBzvASds.js";import"./index-B5sH5m6f.js";import"./ObjectInfoContext-CYsF4dpo.js";import"./index-o5AuT_hg.js";import"./index-BZDIBKFQ.js";import"./index-DWpgm1JM.js";import"./index-pB3ReOnu.js";import"./SpeechContext-cKuR6Onv.js";import"./index-DQTDoKyj.js";import"./index-BIxumPF1.js";import"./index-DKIrFzNR.js";import"./index-Cs7zreYD.js";import"./index-CUNpqF_p.js";import"./AppContext-DX1Wlwzv.js";import"./WsContext-9iT9ZgtG.js";import"./index-BVWtDH-0.js";import"./styled-pLz9Mkps.js";const X={title:"Sidebar",component:p},r={parameters:{layout:"fullscreen",screenshot:{viewport:{width:412,height:700}}},args:{}};var t,o,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen',
+    screenshot: {
+      viewport: {
+        width: 412,
+        // half the width of github README.md on desktop
+        height: 700
+      }
+    }
+  },
+  args: {}
+}`,...(i=(o=r.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const Y=["Sidebar"];export{r as Sidebar,Y as __namedExportsOrder,X as default};
