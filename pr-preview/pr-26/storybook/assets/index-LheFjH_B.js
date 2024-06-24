e54d6d2204e9259fdcb5e79da672443cfaae81e9
norgeous <norgeous@users.noreply.github.com>
2024-06-24 18:27:43 +0000

    Deploy preview for PR 26 🛫

---
+++ pr-preview/pr-26/storybook/assets/index-LheFjH_B.js
@@ -0,0 +1,11 @@
+import{j as o}from"./jsx-runtime-X2b_N9AH.js";import{c as d}from"./index-uyzzwhEt.js";import{c as u}from"./index-D4-6dgTy.js";import{c as x}from"./index-DguYmSp_.js";import{c as f}from"./index-qDzr2xAS.js";import{r}from"./index-uCp2LrAq.js";import{W as l,A as g}from"./AppContext-ZaKAIv11.js";import{c as j}from"./index-Cd27fYzQ.js";import{p as s}from"./styled-components.browser.esm-Drpn4GzW.js";import{b as h}from"./index-BXd8L118.js";const n=()=>{const{progress:e,lastWsMessage:p,isGenerating:t}=r.useContext(l),{executeInterrupt:m,interruptLoading:a,interruptError:c}=r.useContext(g);return t?o.jsx(j,{isLoading:t,status:`${p} ${t&&`${Math.round(e*100)}%`}`,progress:e,onInterrupt:m,interruptLoading:a,interruptError:c}):null};n.__docgenInfo={description:"",methods:[],displayName:"Queue"};const $=s.aside`
+  ${h(1)}
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  height: 100%;
+`,b=s.div`
+  padding: 8px;
+  overflow-y: auto;
+  flex-grow: 1;
+`,i=()=>o.jsxs($,{children:[o.jsx(d,{}),o.jsx(b,{children:o.jsx(u,{})}),o.jsxs(f,{pad:!0,column:!0,children:[o.jsx(n,{}),o.jsx(x,{})]})]}),L=i;i.__docgenInfo={description:"",methods:[],displayName:"Sidebar"};export{L as c};
