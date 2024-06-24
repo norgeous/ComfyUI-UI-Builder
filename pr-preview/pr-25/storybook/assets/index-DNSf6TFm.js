606634eb7c0eba0f742ed6833d9f897668289ee5
norgeous <norgeous@users.noreply.github.com>
2024-06-24 14:24:04 +0000

    Deploy preview for PR 25 🛫

---
+++ pr-preview/pr-25/storybook/assets/index-DNSf6TFm.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-X2b_N9AH.js";import{r as f}from"./index-uCp2LrAq.js";import{P as e}from"./index-D3ylJrlI.js";import{I as c}from"./InputRefContext-CmPhBSwH.js";import{c as m}from"./index-qDzr2xAS.js";import{c as g}from"./index-B-jMyJAp.js";import{p as v}from"./styled-components.browser.esm-Drpn4GzW.js";const x=v.input.attrs({type:"text"})`
+  background: var(--bg3);
+  color: var(--fg3);
+  border: 1px solid var(--bg4);
+
+  display: block;
+  width: 100%;
+  min-width: 100%;
+  box-sizing: border-box;
+  border-radius: var(--radius);
+  padding: 8px 4px;
+  &:focus-visible {
+    outline: 2px solid var(--accent);
+    outline-offset: 2px;
+  }
+`,o=({id:a=void 0,label:i=void 0,info:s=void 0,options:n=[],value:u=void 0,onChange:l=()=>{},children:d=null})=>{const p=f.useContext(c);return r.jsxs(m,{pad:!0,column:!0,gap:"sm",children:[r.jsx(g,{id:a,label:i,info:s,children:d}),r.jsx(x,{ref:p,id:a,value:u,onChange:t=>l(t.target.value),list:n.length?`${a}-list`:void 0}),!!n.length&&r.jsx("datalist",{id:`${a}-list`,children:n.map(t=>r.jsx("option",{"aria-label":t,value:t},t))})]})};o.propTypes={id:e.string,label:e.string,info:e.string,options:e.arrayOf(e.string),value:e.string,onChange:e.func,children:e.node};const C=o;o.__docgenInfo={description:"",methods:[],displayName:"InputText",props:{id:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},label:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},info:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},options:{defaultValue:{value:"[]",computed:!1},description:"auto completions array of strings",type:{name:"arrayOf",value:{name:"string"}},required:!1},value:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},onChange:{defaultValue:{value:"() => {}",computed:!1},description:"",type:{name:"func"},required:!1},children:{defaultValue:{value:"null",computed:!1},description:"",type:{name:"node"},required:!1}}};export{C as c};
--- pr-preview/pr-25/storybook/assets/index.stories-BBzYJr-k.js
+++
@@ -1,8 +0,0 @@
-import{S as a}from"./SimpleStateDecorator-CPTCo3Nu.js";import{c as p}from"./index-DRIOvqJr.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Ct7ZYFme.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-CLhq28W3.js";import"./index-D3ylJrlI.js";import"./index-D-wTJ6ct.js";import"./index-DtWe4Bef.js";import"./floating-ui.react-J0QxEdvZ.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BiKVFdKX.js";import"./index-qDzr2xAS.js";import"./index-DASKFeJk.js";import"./index-8ID9tISY.js";const O={title:"InputTextarea",component:p,decorators:[a]},t={args:{id:"demo",label:"My InputTextarea Label",info:"info text",defaultValue:""}};var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    id: 'demo',
-    label: 'My InputTextarea Label',
-    info: 'info text',
-    defaultValue: ''
-  }
-}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const h=["InputTextarea"];export{t as InputTextarea,h as __namedExportsOrder,O as default};
