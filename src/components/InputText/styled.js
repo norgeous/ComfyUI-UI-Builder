ad5dff07cb310ee28273246d8b759835e576b618
norgeous <daniel@harte.me>
2024-06-24 23:22:02 +0100

    update

--- src/components/InputText/styled.js
+++ src/components/InputText/styled.js
@@ -3,8 +3,8 @@ import { bgfg } from '@/components/GlobalStyles';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Input = styled.input.attrs({ type: 'text' })`
-  ${bgfg(3)}
-  border: 1px solid var(--bg4);
+  ${bgfg(4)}
+  border: 1px solid var(--bg5);
   display: block;
   width: 100%;
   min-width: 100%;
