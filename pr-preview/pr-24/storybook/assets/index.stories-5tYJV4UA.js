d85b212d8f4025e95ddb38ccb05f57884d352bb6
norgeous <norgeous@users.noreply.github.com>
2024-06-24 08:21:29 +0000

    Deploy preview for PR 24 🛫

---
+++ pr-preview/pr-24/storybook/assets/index.stories-5tYJV4UA.js
@@ -0,0 +1,24 @@
+import{S as r}from"./SimpleStateDecorator-Bi8hSyFQ.js";import{c as a}from"./index-BjKMtti2.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Ct7ZYFme.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-gNuICqym.js";import"./index-D3ylJrlI.js";import"./index-D_SX36Yj.js";import"./index-8yc3hoqd.js";import"./floating-ui.react-CDqbCaSC.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BiKVFdKX.js";import"./deepEqual-_sTZaZ0q.js";import"./index-qDzr2xAS.js";import"./index-BZF6B-3v.js";import"./index-DaNusNhd.js";const E={title:"InputSelect",component:a,decorators:[r]},t={args:{id:"demo",label:"My InputSelect Label",info:"help text",defaultValueIndex:2,options:[{label:"Option 1",value:{testname:!1}},{label:"Option 2",value:{testname:!0}},{label:"Option 3 (default)",value:{testname:"foobar"}}]}};var e,o,n;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputSelect Label',
+    info: 'help text',
+    defaultValueIndex: 2,
+    options: [{
+      label: 'Option 1',
+      value: {
+        testname: false
+      }
+    }, {
+      label: 'Option 2',
+      value: {
+        testname: true
+      }
+    }, {
+      label: 'Option 3 (default)',
+      value: {
+        testname: 'foobar'
+      }
+    }]
+  }
+}`,...(n=(o=t.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const j=["InputSelect"];export{t as InputSelect,j as __namedExportsOrder,E as default};
--- pr-preview/pr-24/storybook/assets/index.stories-Crc62ipe.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-X2b_N9AH.js";import{c as o}from"./index-CaMEndn5.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BAMY2Nnw.js";const u={title:"InputWrapper",component:o},r={args:{children:e.jsxs(e.Fragment,{children:[e.jsx("div",{children:"this component should be deleted"}),e.jsxs("div",{children:["we can use ","<Layout />"," instead"]})]})}};var t,s,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: <>
-        <div>this component should be deleted</div>
-        <div>we can use {'<Layout />'} instead</div>
-      </>
-  }
-}`,...(n=(s=r.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const l=["InputWrapper"];export{r as InputWrapper,l as __namedExportsOrder,u as default};
