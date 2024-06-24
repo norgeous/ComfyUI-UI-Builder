ab2d2d9e6aea4c6040852b7813d245a969088867
norgeous <norgeous@users.noreply.github.com>
2024-06-24 12:26:57 +0000

    Deploy preview for PR 25 🛫

--- pr-preview/pr-25/storybook/assets/index.stories-JDPQ8f7t.js
+++
@@ -1,8 +0,0 @@
-import{c as n}from"./index-0_AIZsam.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-DRJiN5Gl.js";import"./index-BiKVFdKX.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BAMY2Nnw.js";import"./index-ENyULWse.js";import"./floating-ui.react-CDqbCaSC.js";import"./index-BOkhicXD.js";const I={title:"InputHeader",component:n},e={args:{id:"test",label:"My InputHeader Label",info:"my info text",children:"extra children"}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    id: 'test',
-    label: 'My InputHeader Label',
-    info: 'my info text',
-    children: 'extra children'
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const b=["InputHeader"];export{e as InputHeader,b as __namedExportsOrder,I as default};
---
+++ pr-preview/pr-25/storybook/assets/index.stories-SbWUrkXB.js
@@ -0,0 +1,13 @@
+import{u as o}from"./index-Ct7ZYFme.js";import{c as i}from"./index-0_AIZsam.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-DRJiN5Gl.js";import"./index-BiKVFdKX.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-ENyULWse.js";import"./floating-ui.react-CDqbCaSC.js";import"./index-BOkhicXD.js";const g={title:"InputHeader",component:i},e={args:{id:"test",label:"My InputHeader Label",info:"Helpful info text about this input",children:"extra children"},play:async({canvasElement:a})=>{await o.hover(a.querySelector("span"))}};var t,r,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    id: 'test',
+    label: 'My InputHeader Label',
+    info: 'Helpful info text about this input',
+    children: 'extra children'
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    await userEvent.hover(canvasElement.querySelector('span'));
+  }
+}`,...(n=(r=e.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const S=["InputHeader"];export{e as InputHeader,S as __namedExportsOrder,g as default};
