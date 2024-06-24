d713ac5c86b09e0d84fc1c6ae53f1e388c83f52d
norgeous <norgeous@users.noreply.github.com>
2024-06-24 14:40:59 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index-DBnD2Hxc.js
@@ -0,0 +1,50 @@
+import{j as o}from"./jsx-runtime-X2b_N9AH.js";import{r as v}from"./index-uCp2LrAq.js";import{P as e}from"./index-D3ylJrlI.js";import{u as h,a as y,b as k,c as j,f as q,s as P}from"./floating-ui.react-J0QxEdvZ.js";import{p as s}from"./styled-components.browser.esm-Drpn4GzW.js";const O=s.button`
+  cursor: pointer;
+  display: block;
+  background: transparent;
+  font-size: inherit;
+  border: none;
+  color: var(--fg-mute);
+  border-radius: var(--radius);
+  margin: 0;
+  padding: 0;
+  &:hover {
+    color: var(--fg0);
+  }
+  &:focus-visible {
+    outline: 2px solid var(--accent);
+    outline-offset: -2px;
+  }
+`,w=s.span`
+  position: absolute;
+  font-size: 0.75rem; // 12px
+  background: var(--bg1);
+  color: var(--fg1);
+  text-align: center;
+  border-radius: 6px;
+  padding: 8px;
+  z-index: 1;
+  top: 100%;
+  left: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`,C=s.button`
+  cursor: pointer;
+  display: block;
+  background: transparent;
+  border: none;
+  color: var(--fg-mute);
+  border-radius: var(--radius);
+  margin: 0;
+  padding: 0;
+  &:hover {
+    color: var(--fg0);
+  }
+  &:focus-visible {
+    outline: 2px solid var(--accent);
+    outline-offset: -2px;
+  }
+  width: 100%;
+  text-align: left;
+`,t=({className:l=void 0,children:p=null,options:d=[],...c})=>{const[n,a]=v.useState(!1),{refs:i,floatingStyles:u,context:f}=h({placement:"auto",middleware:[j(),q(),P({padding:14})],open:n,onOpenChange:r=>{a(r)}}),m=y(f),{getReferenceProps:g,getFloatingProps:x}=k([m]);return o.jsxs(o.Fragment,{children:[o.jsx(O,{...c,className:l,ref:i.setReference,onClick:()=>a(!n),...g(),children:p}),n&&o.jsx(w,{ref:i.setFloating,style:u,...x(),children:d.map(({label:r,onClick:b})=>o.jsx("div",{children:o.jsx(C,{onClick:b,children:r})},r))})]})};t.propTypes={className:e.string,lm:e.bool,wide:e.bool,children:e.node,options:e.arrayOf(e.shape({label:e.string,onClick:e.func}))};const R=t;t.__docgenInfo={description:"",methods:[],displayName:"PopMenu",props:{className:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},children:{defaultValue:{value:"null",computed:!1},description:"",type:{name:"node"},required:!1},options:{defaultValue:{value:"[]",computed:!1},description:"",type:{name:"arrayOf",value:{name:"shape",value:{label:{name:"string",required:!1},onClick:{name:"func",required:!1}}}},required:!1},lm:{description:"",type:{name:"bool"},required:!1},wide:{description:"",type:{name:"bool"},required:!1}}};export{R as c};
