ae2e15075e01590c3a8683d8ea4396f1e4ab8c3d
norgeous <daniel@harte.me>
2024-06-25 15:23:19 +0100

    nicer warning

--- src/components/InputSelectCkpt/index.jsx
+++ src/components/InputSelectCkpt/index.jsx
@@ -1,9 +1,9 @@
 import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ComfyBridgeContext from '@/contexts/ComfyBridgeContext';
-import Layout from '@/components/Layout';
 import InputText from '@/components/InputText';
-import ErrorText from '@/components/ErrorText';
+import Tooltip from '@/components/Tooltip';
+import { SpinnerIcon, WarningIcon } from '@/components/Icons';
 
 const InputSelectCkpt = ({ id = undefined, value = undefined, ...props }) => {
   const { objectInfo } = useContext(ComfyBridgeContext);
@@ -12,16 +12,19 @@ const InputSelectCkpt = ({ id = undefined, value = undefined, ...props }) => {
     objectInfo?.data?.CheckpointLoaderSimple.input.required.ckpt_name[0];
 
   return (
-    <Layout pad column gap="sm">
-      <InputText
-        {...props} // eslint-disable-line react/jsx-props-no-spreading
-        id={id}
-        value={value}
-        options={ckptOptions || []}
-      />
-      {objectInfo?.loading && 'LOADING'}
-      {!ckptOptions && <ErrorText>No checkpoints available</ErrorText>}
-    </Layout>
+    <InputText
+      {...props} // eslint-disable-line react/jsx-props-no-spreading
+      id={id}
+      value={value}
+      options={ckptOptions || []}
+    >
+      {!ckptOptions && (
+        <Tooltip text="No checkpoints available">
+          <WarningIcon />
+        </Tooltip>
+      )}
+      {objectInfo?.loading && <SpinnerIcon />}
+    </InputText>
   );
 };
 
