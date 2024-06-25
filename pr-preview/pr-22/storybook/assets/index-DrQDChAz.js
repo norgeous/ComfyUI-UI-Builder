b538000358ab5a14875f6581321e46b92af25cb8
norgeous <norgeous@users.noreply.github.com>
2024-06-25 12:30:30 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index-DrQDChAz.js
@@ -0,0 +1,33 @@
+import{j as o}from"./jsx-runtime-X2b_N9AH.js";import{r as e}from"./index-uCp2LrAq.js";import{C as k}from"./ComfyBridgeContext-Dy9XN-KW.js";import{W as w,A as I}from"./AppContext-ZaKAIv11.js";import{F as v}from"./FormContext-7paO4gYw.js";import{C as B}from"./index-BUFFlF7z.js";import{O as D}from"./ObjectInfoContext-CYsF4dpo.js";import{G as S,d as W,B as U}from"./index-BiKVFdKX.js";import{c as $}from"./index-0KXYed6g.js";import{c as O}from"./index-BC5bIBz4.js";import{c as _}from"./index-Bs-Be9SB.js";import{p as s}from"./styled-components.browser.esm-Drpn4GzW.js";const E=s.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 4px;
+  color: var(--fg-mute);
+  background: var(--bg1);
+  padding: 4px;
+`,c=s.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 4px;
+  overflow: hidden;
+  flex-shrink: ${({$shrinkable:a})=>a?1:0};
+`,G=s.div`
+  font-size: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`,l=s.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: inherit;
+  &:hover {
+    color: var(--fg0);
+  }
+  &:focus-visible {
+    outline: 2px solid var(--accent);
+    outline-offset: -2px;
+  }
+`,f=()=>{var r,i;const a=e.useContext(k),{comfyUrl:d}=e.useContext(w),{formState:p}=e.useContext(v),{config:{baseWorkflow:m}}=e.useContext(B),{objectInfoLoading:x,objectInfoError:b,objectInfo:u}=e.useContext(D),{adapted:g,adaptedComfyWorkflow:C,bodyData:y}=e.useContext(I),[n,t]=e.useState("comfyBridge"),h=[{label:"comfyBridge",onClick:()=>t("comfyBridge")},{label:"formState",onClick:()=>t("formState")},{label:"adapted",onClick:()=>t("adapted")},{label:"baseWorkflow",onClick:()=>t("baseWorkflow")},{label:"objectInfo",onClick:()=>t("objectInfo")},{label:"adaptedComfyWorkflow",onClick:()=>t("adaptedComfyWorkflow")},{label:"bodyData",onClick:()=>t("bodyData")}],j={comfyBridge:a,formState:p,adapted:g,baseWorkflow:m,objectInfo:{objectInfoLoading:x,objectInfoError:b,objectInfo:u},adaptedComfyWorkflow:C,bodyData:y}[n];return o.jsxs(E,{children:[o.jsxs(c,{$shrinkable:!0,children:[o.jsx($,{status:(r=a.ws)==null?void 0:r.status}),o.jsxs(G,{children:[(i=a.ws)==null?void 0:i.statusText," ",d]})]}),o.jsxs(c,{children:[o.jsx(l,{href:"https://github.com/norgeous/ComfyUI-UI-Builder/",target:"_blank",children:o.jsx(S,{"aria-label":"Github"})}),o.jsx(l,{href:"https://norgeous.github.io/ComfyUI-UI-Builder/develop/storybook/",target:"_blank",children:o.jsx(W,{"aria-label":"Storybook"})}),o.jsx(O,{"aria-label":"Debug Data Options",options:h,children:o.jsx(U,{})}),n&&o.jsx(_,{onClick:()=>t(),data:j})]})]})},P=f;f.__docgenInfo={description:"",methods:[],displayName:"StatusBar"};export{P as c};
---
+++ pr-preview/pr-22/storybook/assets/index.stories-BGlfuIsO.js
@@ -0,0 +1,45 @@
+import{j as r}from"./jsx-runtime-X2b_N9AH.js";import{c as e}from"./index-BQhqkRiT.js";import{c as n}from"./index-Djfv3WPi.js";import{p as s}from"./styled-components.browser.esm-Drpn4GzW.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./AppContext-ZaKAIv11.js";import"./index-AVQuQ6uG.js";import"./index-D3ylJrlI.js";import"./index-DrQDChAz.js";import"./ComfyBridgeContext-Dy9XN-KW.js";import"./FormContext-7paO4gYw.js";import"./index-BUFFlF7z.js";import"./ObjectInfoContext-CYsF4dpo.js";import"./index-BiKVFdKX.js";import"./index-0KXYed6g.js";import"./index-C_MwD-aL.js";import"./floating-ui.react-ByhEbrkY.js";import"./index-BOkhicXD.js";import"./index-BC5bIBz4.js";import"./index-Bs-Be9SB.js";import"./index-0vOIQXrf.js";import"./index-B6_q2n5M.js";import"./InputRefContext-CmPhBSwH.js";import"./deepEqual-_sTZaZ0q.js";import"./index-qDzr2xAS.js";import"./index--vyq1y6r.js";import"./index-BwISzhqP.js";import"./index-BZ961Q6N.js";import"./index-D1ZOvgUU.js";import"./index-BqJ66KCm.js";import"./index-PH288ROH.js";import"./index-DaRQaGy3.js";import"./index-DvrFgKXm.js";import"./index-WaFSqqId.js";import"./index-VUqoWtxM.js";import"./index-B7Qr_zHC.js";import"./index-Dj4rqja7.js";import"./index-BZDIBKFQ.js";import"./index-BESrCSE-.js";import"./index-B4qol5yP.js";import"./SpeechContext-cKuR6Onv.js";import"./index-xW2h1PdW.js";import"./index-VKE47QHs.js";import"./index-CiLHjC9D.js";import"./index-DHOpqfxG.js";import"./index-CapvPVBA.js";import"./index-BmL8NL8F.js";import"./styled-pLz9Mkps.js";import"./index-BAMY2Nnw.js";const a=s.div`
+  display: grid;
+  height: 100svh;
+
+  // portrait first
+  grid-template-rows: minmax(50%, auto) minmax(auto, 400px);
+
+  // landscape overrides
+  @media (min-aspect-ratio: 1/1) {
+    grid-template-rows: none;
+    grid-template-columns: minmax(auto, 460px) minmax(50%, auto);
+    & > :first-child {
+      order: 2; // change the order on landscape, so that sidebar is first
+    }
+  }
+`,m=()=>r.jsxs(a,{children:[r.jsx(e,{}),r.jsx(n,{})]}),d=m;m.__docgenInfo={description:"",methods:[],displayName:"App"};const nt={title:"App",component:d},t={parameters:{layout:"fullscreen",screenshot:{viewport:{width:830,height:600},variants:{small:{viewport:{width:412,height:700}},small_dark:{viewport:{width:412,height:700},forceDark:!0}}}},args:{}};var o,i,p;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen',
+    screenshot: {
+      viewport: {
+        width: 830,
+        // the width of github README.md on desktop
+        height: 600
+      },
+      variants: {
+        small: {
+          viewport: {
+            width: 412,
+            // half the width of github README.md on desktop
+            height: 700
+          }
+        },
+        small_dark: {
+          viewport: {
+            width: 412,
+            // half the width of github README.md on desktop
+            height: 700
+          },
+          forceDark: true
+        }
+      }
+    }
+  },
+  args: {}
+}`,...(p=(i=t.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const st=["App"];export{t as App,st as __namedExportsOrder,nt as default};
