0425e8ec92b41919752cb864bbb6232f0daf0dc8
norgeous <norgeous@users.noreply.github.com>
2024-06-25 11:48:28 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index-B6_q2n5M.js
@@ -0,0 +1,14 @@
+import{j as r}from"./jsx-runtime-X2b_N9AH.js";import{r as c}from"./index-uCp2LrAq.js";import{P as e}from"./index-D3ylJrlI.js";import{I as v}from"./InputRefContext-CmPhBSwH.js";import{d as g}from"./deepEqual-_sTZaZ0q.js";import{c as x}from"./index-qDzr2xAS.js";import{c as y}from"./index--vyq1y6r.js";import{p as b}from"./styled-components.browser.esm-Drpn4GzW.js";import{b as h}from"./index-BUFFlF7z.js";const q=b.select`
+  ${h(4)}
+  border: 1px solid var(--bg5);
+  display: block;
+  width: 100%;
+  min-width: 100%;
+  box-sizing: border-box;
+  border-radius: var(--radius);
+  padding: 8px 4px;
+  &:focus-visible {
+    outline: 2px solid var(--accent);
+    outline-offset: 2px;
+  }
+`,t=({id:i=void 0,label:l=void 0,info:u=void 0,options:n=[],value:o=void 0,onChange:d=()=>{},children:s=null})=>{const p=c.useContext(v),f=n.findIndex(a=>a.value===o||g(a.value,o));return r.jsxs(x,{pad:!0,column:!0,gap:"sm",children:[r.jsx(y,{id:i,label:l,info:u,children:s}),r.jsx(q,{ref:p,id:i,value:f,onChange:a=>d(n[a.target.value].value),children:n.map(({label:a},m)=>r.jsx("option",{value:String(m),children:a},a))})]})};t.propTypes={id:e.string,label:e.string,info:e.string,options:e.arrayOf(e.shape({label:e.string,value:e.any})),value:e.any,onChange:e.func,children:e.node};const P=t;t.__docgenInfo={description:"",methods:[],displayName:"InputSelect",props:{id:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},label:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},info:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},options:{defaultValue:{value:"[]",computed:!1},description:"",type:{name:"arrayOf",value:{name:"shape",value:{label:{name:"string",required:!1},value:{name:"any",required:!1}}}},required:!1},value:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"any"},required:!1},onChange:{defaultValue:{value:"() => {}",computed:!1},description:"",type:{name:"func"},required:!1},children:{defaultValue:{value:"null",computed:!1},description:"",type:{name:"node"},required:!1}}};export{P as c};
---
+++ pr-preview/pr-22/storybook/assets/index.stories-Dlxl5u3X.js
@@ -0,0 +1,10 @@
+import{f as s,u as a}from"./index-Ct7ZYFme.js";import{c as p}from"./index-D1ZOvgUU.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-BqJ66KCm.js";import"./index-C_MwD-aL.js";import"./floating-ui.react-ByhEbrkY.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BUFFlF7z.js";import"./index-BiKVFdKX.js";const k={title:"ResetButton",component:p},t={args:{onClick:s()},play:async({canvasElement:n})=>{await a.hover(n.querySelector("span"))}};var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    onClick: fn()
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    await userEvent.hover(canvasElement.querySelector('span'));
+  }
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const q=["ResetButton"];export{t as ResetButton,q as __namedExportsOrder,k as default};
