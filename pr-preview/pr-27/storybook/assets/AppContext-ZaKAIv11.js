c6155a10807101f1f88279c9b589d048bf6985e8
norgeous <norgeous@users.noreply.github.com>
2024-06-27 20:37:48 +0000

    Deploy preview for PR 27 🛫

---
+++ pr-preview/pr-27/storybook/assets/AppContext-ZaKAIv11.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-uCp2LrAq.js";const o=t.createContext({comfyUrl:void 0,isGenerating:!1,progress:0,output:void 0}),n=t.createContext({});export{n as A,o as W};
---
+++ pr-preview/pr-27/storybook/assets/index.stories-BBi2uB8I.js
@@ -0,0 +1,29 @@
+import{S as o}from"./SimpleStateDecorator-ICVuba-O.js";import{c as m}from"./index-BqsvMT8v.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Ct7ZYFme.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-CvHBa1Gc.js";import"./InputRefContext-CmPhBSwH.js";import"./index-D1ZOvgUU.js";import"./index-D3ylJrlI.js";import"./index-BqJ66KCm.js";import"./index-C_MwD-aL.js";import"./floating-ui.react-ByhEbrkY.js";import"./index-BOkhicXD.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BUFFlF7z.js";import"./index-BiKVFdKX.js";import"./deepEqual-_sTZaZ0q.js";import"./index-Duqe0ScD.js";import"./index--vyq1y6r.js";import"./index-BwISzhqP.js";const D={title:"InputCheckbox",component:m,decorators:[o]},e={args:{id:"demo",info:"helpful text...",label:"LCM",defaultValueIndex:1,options:[{label:"Off",value:{isLcm:!1,baseSteps:14,stepMultiplier:14,cfg:6.5,samplerName:"dpmpp_2m",scheduler:"exponential"}},{label:"On",value:{isLcm:!0,baseSteps:6,stepMultiplier:4,cfg:2.5,samplerName:"lcm",scheduler:"normal"}}]}};var t,r,p;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    info: 'helpful text...',
+    label: 'LCM',
+    defaultValueIndex: 1,
+    options: [{
+      label: 'Off',
+      value: {
+        isLcm: false,
+        baseSteps: 14,
+        stepMultiplier: 14,
+        cfg: 6.5,
+        samplerName: 'dpmpp_2m',
+        scheduler: 'exponential'
+      }
+    }, {
+      label: 'On',
+      value: {
+        isLcm: true,
+        baseSteps: 6,
+        stepMultiplier: 4,
+        cfg: 2.5,
+        samplerName: 'lcm',
+        scheduler: 'normal'
+      }
+    }]
+  }
+}`,...(p=(r=e.parameters)==null?void 0:r.docs)==null?void 0:p.source}}};const E=["InputCheckbox"];export{e as InputCheckbox,E as __namedExportsOrder,D as default};
