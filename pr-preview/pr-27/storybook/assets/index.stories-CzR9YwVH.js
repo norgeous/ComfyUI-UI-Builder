aae1e1bdc448dae548dc40fab4cef8c1025a2cfc
norgeous <norgeous@users.noreply.github.com>
2024-06-27 21:49:33 +0000

    Remove preview for PR 27 🛬

--- pr-preview/pr-27/storybook/assets/index.stories-CzR9YwVH.js
+++
@@ -1,8 +0,0 @@
-import{c as o}from"./index-B4cvI_Q2.js";import"./jsx-runtime-X2b_N9AH.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-BiKVFdKX.js";import"./styled-components.browser.esm-Drpn4GzW.js";import"./index-BAMY2Nnw.js";import"./index-BUFFlF7z.js";const u={title:"Collapsable",component:o},a={args:{title:"Collapsable Title",children:"Candy cake marshmallow cupcake chocolate bar tart toffee gummi     bears bonbon. Cake cotton candy apple pie tart chupa chups pudding dessert     wafer bear claw."}};var e,r,t;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    title: 'Collapsable Title',
-    children: \`Candy cake marshmallow cupcake chocolate bar tart toffee gummi \\
-    bears bonbon. Cake cotton candy apple pie tart chupa chups pudding dessert \\
-    wafer bear claw.\`
-  }
-}`,...(t=(r=a.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const h=["Collapsable"];export{a as Collapsable,h as __namedExportsOrder,u as default};
