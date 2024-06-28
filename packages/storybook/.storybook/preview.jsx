7119ad945d535da6e11217b1b4419032452676d9
norgeous <daniel@harte.me>
2024-06-28 12:47:46 +0100

    try to mock ws

--- packages/storybook/.storybook/preview.jsx
+++ packages/storybook/.storybook/preview.jsx
@@ -1,8 +1,51 @@
 import { initialize, mswLoader } from 'msw-storybook-addon';
 import { withScreenshot } from 'storycap';
+import { ws } from 'msw';
 import ProjectDecorator from '../ProjectDecorator';
 
-initialize(); // Initialize MSW
+const service = ws.link('*');
+
+initialize({}, [
+  // { run: (...d) => console.log(d[0].request.url) },
+  // {
+  //   run:
+  // },
+
+  service.on('connection', ({ client }) => {
+    console.log('outgoing ws connection:', client.url);
+  }),
+]);
+
+// console.log({ service });
+
+// service.on('connection', ({ client }) => {
+//   console.log('!');
+//   // throw new Error('aaaaaaaaaaaaah');
+//   client.addEventListener('message', async event => {
+//     // const { username, message } = JSON.parse(event.data);
+//     // First, broadcast the client message to other clients.
+//     // service.broadcastExcept(client, formatMessage(username, message));
+//     // await delay(0);
+//     // Then, respond to the message for everyone to see.
+//     // service.broadcast(formatMessage('msw-bot', `Welcome, ${username}!`));
+//     // setTimeout(() => {
+//     //   service.broadcast(formatMessage('msw-bot', 'How are you?'));
+//     // }, 1500);
+//   });
+// });
+
+// {
+//   // onUnhandledRequest: ({ headers, method, url }) => {
+//   //   if (headers.get('User-Agent') !== 'supertest') {
+//   //     throw new Error(`Unhandled ${method} request to ${url}`);
+//   //   }
+//   // },
+// },
+// [
+//   {
+//     run: () =>
+//   },
+// ], // Initialize MSW
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
@@ -31,6 +74,19 @@ const preview = {
       },
       variants: {
         dark: { forceDark: true },
+      },
+    },
+
+    msw: {
+      handlers: {
+        mytung: {
+          run: () => {
+            service.on('connection', ({ client }) => {
+              console.log('outgoing ws connection:', client.url);
+            });
+            return true;
+          },
+        },
       },
     },
   },
