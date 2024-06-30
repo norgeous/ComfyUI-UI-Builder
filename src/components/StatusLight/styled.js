95e7237507a286869b07bc69e3abe42a86079a66
norgeous <daniel@harte.me>
2024-06-30 12:27:03 +0100

    swap status emoji for css gradient circle

--- src/components/StatusLight/styled.js
+++ src/components/StatusLight/styled.js
@@ -2,6 +2,29 @@ import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Light = styled.span`
-  color: white;
-  user-select: none;
+  width: 12px;
+  height: 12px;
+  border: 1px solid var(--fg0);
+  border-radius: 50%; // circle
+  transition: background-position 250ms ease-in-out;
+  background: linear-gradient(
+    -180deg,
+    grey,
+    black,
+    deepskyblue,
+    blue,
+    chartreuse,
+    green,
+    crimson,
+    darkred
+  );
+  background-size: 100% 700%;
+  background-repeat: no-repeat;
+  background-position: ${({ status }) =>
+    ({
+      DEFAULT: '0% 0%',
+      CONNECTING: '0% 33.33%',
+      CONNECTED: '0% 66.66%',
+      DISCONNECTED: '0% 100%',
+    })[status]};
 `;
