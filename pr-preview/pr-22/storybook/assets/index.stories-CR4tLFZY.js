ef4812dba1b69ed73aede5b25e3f4d04b18f08da
norgeous <norgeous@users.noreply.github.com>
2024-06-30 20:09:22 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-CR4tLFZY.js
@@ -0,0 +1,12 @@
+import{w as i,u as s}from"./index-PX7pnrjg.js";import{c as m}from"./index-DiHkjeLp.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./index-Bm7KGtcn.js";import"./index-D3ylJrlI.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-NmZOhbA9.js";import"./FormContext-BvePLfBF.js";import"./index-CXGN0Eu6.js";import"./index-VWZqVgFq.js";import"./index-DcJkU15T.js";import"./index-DmipQToa.js";import"./floating-ui.react-BQk9wdfR.js";import"./index-B8XB3FuZ.js";import"./index-puLRY7LT.js";import"./index-BK5_XIt8.js";const q={title:"MainContent",component:m},t={parameters:{layout:"fullscreen"},args:{},play:async({canvasElement:a})=>{const e=i(a);await s.click(e.getByRole("button"))}};var r,o,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {},
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('button'));
+  }
+}`,...(n=(o=t.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const z=["MainContent"];export{t as MainContent,z as __namedExportsOrder,q as default};
