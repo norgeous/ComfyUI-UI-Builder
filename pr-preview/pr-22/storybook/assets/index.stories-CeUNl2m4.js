d4a5ee9ea15b19784369a0b614b7349d7e5f3340
norgeous <norgeous@users.noreply.github.com>
2024-06-30 14:58:42 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-CeUNl2m4.js
@@ -0,0 +1,8 @@
+import{f as e}from"./index-PX7pnrjg.js";import{c as s}from"./index--wt0rWbF.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./styled-components.browser.esm-Cm6OPFyG.js";const x={title:"Debug",component:s},o={args:{onClick:e(),data:{foo:"bar"}}};var r,t,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    onClick: fn(),
+    data: {
+      foo: 'bar'
+    }
+  }
+}`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const D=["Debug"];export{o as Debug,D as __namedExportsOrder,x as default};
