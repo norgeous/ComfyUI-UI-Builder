85d2929ffca02d76c6c3b15dee2d96622adf6493
norgeous <daniel@harte.me>
2024-07-02 23:24:30 +0100

    Update comfyMock.js

--- packages/comfybridge/comfyMock.js
+++ packages/comfybridge/comfyMock.js
@@ -19,56 +19,54 @@ const mockJobEvents = [
 const service = ws.link(`ws://${window.location.host}/ws`);
 const wsMock = service.on('connection', ({ client, ...other }) => {});
 
-const objectInfoMock = http.get(
-  `http://${window.location.host}/object_info`,
-  () =>
-    HttpResponse.json({
-      CheckpointLoaderSimple: {
-        input: {
-          required: {
-            ckpt_name: [
-              [
-                'mock-model-one-XL.safetensors',
-                'mock-two.SDXL.ckpt',
-                'my-checkpoint.SD15.safetensors',
-                'SDXL/my-checkpoint.SDXL.safetensors',
-              ],
+const objectInfoMock = http.get(`${window.location.origin}/object_info`, () =>
+  HttpResponse.json({
+    CheckpointLoaderSimple: {
+      input: {
+        required: {
+          ckpt_name: [
+            [
+              'mock-model-one-XL.safetensors',
+              'mock-two.SDXL.ckpt',
+              'my-checkpoint.SD15.safetensors',
+              'SDXL/my-checkpoint.SDXL.safetensors',
             ],
-          },
+          ],
         },
       },
-      LoraLoader: {
-        input: {
-          required: {
-            lora_name: [
-              [
-                'mock-lora-1.safetensors',
-                'mock-lora-2.safetensors',
-                'mock-lora-3.safetensors',
-              ],
+    },
+    LoraLoader: {
+      input: {
+        required: {
+          lora_name: [
+            [
+              'mock-lora-1.safetensors',
+              'mock-lora-2.safetensors',
+              'mock-lora-3.safetensors',
             ],
-          },
+          ],
         },
       },
-      LoadImage: {
-        input: {
-          required: {
-            image: [
-              [
-                'image1.jpg',
-                'image2.jpg',
-                'image3.jpg',
-                'image4.jpg',
-                'image5.jpg',
-              ],
+    },
+    LoadImage: {
+      input: {
+        required: {
+          image: [
+            [
+              'image1.jpg',
+              'image2.jpg',
+              'image3.jpg',
+              'image4.jpg',
+              'image5.jpg',
             ],
-          },
+          ],
         },
       },
-    }),
+    },
+  }),
 );
 
-const promptMock = http.post(`http://${window.location.host}/prompt`, () => {
+const promptMock = http.post(`${window.location.origin}/prompt`, () => {
   setInterval(() => {
     service.broadcast(JSON.stringify({ mate: new Date().getTime() }));
   }, 1000);
@@ -80,7 +78,7 @@ const promptMock = http.post(`http://${window.location.host}/prompt`, () => {
   });
 });
 
-const interruptMock = http.get(`http://${window.location.host}/interrupt`, () =>
+const interruptMock = http.get(`${window.location.origin}/interrupt`, () =>
   // find job id in the queue and cancel the interval?
   HttpResponse.json({
     id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
@@ -89,14 +87,12 @@ const interruptMock = http.get(`http://${window.location.host}/interrupt`, () =>
   }),
 );
 
-const imageMock = http.get(
-  `http://${window.location.host}/view`,
-  ({ request }) => {
-    const url = new URL(request.url);
-    const width = url.searchParams.get('width') || 512;
-    const height = url.searchParams.get('height') || 512;
-    const fontSize = Math.min(width, height) / 2;
-    const svg = `
+const imageMock = http.get(`${window.location.origin}/view`, ({ request }) => {
+  const url = new URL(request.url);
+  const width = url.searchParams.get('width') || 512;
+  const height = url.searchParams.get('height') || 512;
+  const fontSize = Math.min(width, height) / 2;
+  const svg = `
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 ${width} ${height}"
@@ -119,12 +115,11 @@ const imageMock = http.get(
     </svg>
   `;
 
-    return HttpResponse.xml(svg, {
-      headers: {
-        'Content-Type': 'image/svg+xml',
-      },
-    });
-  },
-);
+  return HttpResponse.xml(svg, {
+    headers: {
+      'Content-Type': 'image/svg+xml',
+    },
+  });
+});
 
 export default [wsMock, objectInfoMock, promptMock, interruptMock, imageMock];
