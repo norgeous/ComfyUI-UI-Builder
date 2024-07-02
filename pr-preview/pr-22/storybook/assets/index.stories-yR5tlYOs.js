eeac15055a932161f6dee5fe9151e7be1d30cfff
norgeous <norgeous@users.noreply.github.com>
2024-07-02 14:25:22 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-yR5tlYOs.js
@@ -0,0 +1,16 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{p as a}from"./styled-components.browser.esm-Cm6OPFyG.js";import{i as p}from"./index-n3Wug2HJ.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BAMY2Nnw.js";const m=a.div`
+  display: flex;
+  gap: 8px;
+  font-size: 30px;
+  flex-wrap: wrap;
+`,j={title:"Icons"},e={parameters:{viewport:{width:214,height:214}},render:()=>t.jsx(m,{children:Object.entries(p).map(([r,i])=>t.jsx(i,{title:r},r))})};var n,o,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  parameters: {
+    viewport: {
+      width: 214,
+      height: 214
+    }
+  },
+  render: () => <Container>
+      {Object.entries(icons).map(([name, Icon]) => <Icon key={name} title={name} />)}
+    </Container>
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const w=["Icons"];export{e as Icons,w as __namedExportsOrder,j as default};
