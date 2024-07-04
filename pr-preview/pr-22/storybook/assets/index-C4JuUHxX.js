31af3c590673fc52e28237263be7eaa22066745a
norgeous <norgeous@users.noreply.github.com>
2024-07-04 22:29:11 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index-C4JuUHxX.js
@@ -0,0 +1,15 @@
+import{j as i}from"./jsx-runtime-Nms4Y4qS.js";import{P as e}from"./index-D3ylJrlI.js";import"./index-D0Aq9VeK.js";import"./index-Dmvdrk1-.js";import"./index-DZgcxs9s.js";import"./index-CHiMFBbc.js";import"./styled-DXX43EGL.js";import{p as n}from"./styled-components.browser.esm-Cm6OPFyG.js";n.div`
+  font-size: 10px;
+  overflow: hidden;
+  flex-grow: 1;
+
+  & div {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;const r=({id:l=void 0,loading:p=!1,error:s=void 0,data:f=void 0,status:m=void 0,progress:c=0,onInterrupt:v=()=>{},interruptLoading:g=!1,interruptError:y="",onRemove:q=()=>{},promptId:t=void 0,type:u=void 0,node:a=void 0,value:d=void 0,max:o=void 0})=>i.jsx("pre",{children:JSON.stringify({promptId:t,type:u,node:a,value:d,max:o},null,2)});r.propTypes={id:e.string,loading:e.bool,error:e.string,data:e.any,status:e.string,progress:e.number,onInterrupt:e.func,interruptLoading:e.bool,interruptError:e.string,onRemove:e.func};const j=r;r.__docgenInfo={description:"",methods:[],displayName:"QueueItem",props:{id:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},loading:{defaultValue:{value:"false",computed:!1},description:"",type:{name:"bool"},required:!1},error:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},data:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"any"},required:!1},status:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"string"},required:!1},progress:{defaultValue:{value:"0",computed:!1},description:"",type:{name:"number"},required:!1},onInterrupt:{defaultValue:{value:"() => {}",computed:!1},description:"",type:{name:"func"},required:!1},interruptLoading:{defaultValue:{value:"false",computed:!1},description:"",type:{name:"bool"},required:!1},interruptError:{defaultValue:{value:"''",computed:!1},description:"",type:{name:"string"},required:!1},onRemove:{defaultValue:{value:"() => {}",computed:!1},description:"",type:{name:"func"},required:!1},promptId:{defaultValue:{value:"undefined",computed:!0},required:!1},type:{defaultValue:{value:"undefined",computed:!0},required:!1},node:{defaultValue:{value:"undefined",computed:!0},required:!1},value:{defaultValue:{value:"undefined",computed:!0},required:!1},max:{defaultValue:{value:"undefined",computed:!0},required:!1}}};export{j as c};
--- pr-preview/pr-22/storybook/assets/index.stories-BaHHdQFE.js
+++
@@ -1,6 +0,0 @@
-import{c as m}from"./index-CANsewyu.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-wo4bmeOh.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-BAMY2Nnw.js";import"./index-Dx3lFgnb.js";import"./index-D3ylJrlI.js";import"./InputRefContext-5kgal7hN.js";import"./deepEqual-_sTZaZ0q.js";import"./index-D0Aq9VeK.js";import"./index-DYpCCn-Y.js";import"./index-CWuWN1S5.js";import"./index-n3Wug2HJ.js";import"./index-CHiMFBbc.js";import"./floating-ui.react-BQk9wdfR.js";import"./index-B8XB3FuZ.js";const S={title:"FormHeader",component:m},r={parameters:{layout:"fullscreen"},args:{}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  parameters: {
-    layout: 'fullscreen'
-  },
-  args: {}
-}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const b=["FormHeader"];export{r as FormHeader,b as __namedExportsOrder,S as default};
