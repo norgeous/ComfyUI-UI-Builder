6515babc93f12618f39a893d5f02dfb94e49a073
norgeous <daniel@harte.me>
2024-07-04 10:56:02 +0100

    Update index.js

--- packages/comfybridge/index.js
+++ packages/comfybridge/index.js
@@ -30,7 +30,6 @@ const comfybridge = ({ onChange = () => {} }) => {
     queue: [],
   };
   const updateState = (key, newData) => {
-    // console.log({ key, ...newData });
     state[key] = { ...state[key], ...newData };
     onChange(state);
   };
