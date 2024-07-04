2115529cd42dde7d32c968b6e479d6357651031f
norgeous <norgeous@users.noreply.github.com>
2024-07-04 13:19:00 +0000

    Deploy preview for PR 22 🛫

--- pr-preview/pr-22/storybook/assets/index.stories-Dmx-ikTi.js
+++ pr-preview/pr-22/storybook/assets/index.stories-Dmx-ikTi.js
@@ -1,4 +1,4 @@
-import{f as d,w as c,u as i}from"./index-PX7pnrjg.js";import{c as L}from"./index-CX80jRvq.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-D0Aq9VeK.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-wo4bmeOh.js";import"./index-Dmvdrk1-.js";import"./index-n3Wug2HJ.js";import"./index-CHiMFBbc.js";import"./floating-ui.react-BQk9wdfR.js";import"./index-B8XB3FuZ.js";import"./styled-DXX43EGL.js";const N={title:"QueueItem",component:L},n={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!1,error:"469",data:{error:{message:"There was a problem"}}}},r={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!0,status:"Running…",progress:.25,onInterrupt:d()},play:async({canvasElement:a})=>{const e=c(a);await i.hover(e.getByRole("button"))}},t={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!0,status:"Running…",progress:.5,onInterrupt:d(),interruptLoading:!0},play:async({canvasElement:a})=>{const e=c(a);await i.hover(e.getByRole("button"))}},s={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!0,status:"Running…",progress:.75,onInterrupt:d(),interruptError:"There was a problem with the interrupt"},play:async({canvasElement:a})=>{const e=c(a);await i.hover(e.getByRole("button"))}},o={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!1,status:"Done!",progress:1,onInterrupt:d()},play:async({canvasElement:a})=>{const e=c(a);await i.hover(e.getByRole("button"))}};var p,u,b;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+import{w as d,u as c,f as i}from"./index-PX7pnrjg.js";import{c as L}from"./index-CX80jRvq.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./index-D0Aq9VeK.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-wo4bmeOh.js";import"./index-Dmvdrk1-.js";import"./index-n3Wug2HJ.js";import"./index-CHiMFBbc.js";import"./floating-ui.react-BQk9wdfR.js";import"./index-B8XB3FuZ.js";import"./styled-DXX43EGL.js";const N={title:"QueueItem",component:L},n={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!1,error:"469",data:{error:{message:"There was a problem"}}},play:async({canvasElement:a})=>{const e=d(a);await c.hover(e.getByRole("button"))}},r={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!0,status:"Running…",progress:.25,onInterrupt:i()},play:async({canvasElement:a})=>{const e=d(a);await c.hover(e.getByRole("button"))}},t={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!0,status:"Running…",progress:.5,onInterrupt:i(),interruptLoading:!0},play:async({canvasElement:a})=>{const e=d(a);await c.hover(e.getByRole("button"))}},s={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!0,status:"Running…",progress:.75,onInterrupt:i(),interruptError:"There was a problem with the interrupt"},play:async({canvasElement:a})=>{const e=d(a);await c.hover(e.getByRole("button"))}},o={args:{id:"baadb00b-babe-462f-cafe-d34dd00dcafe",loading:!1,status:"Done!",progress:1,onInterrupt:i()}};var p,u,b;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
   args: {
     id: 'baadb00b-babe-462f-cafe-d34dd00dcafe',
     loading: false,
@@ -8,6 +8,12 @@ import{f as d,w as c,u as i}from"./index-PX7pnrjg.js";import{c as L}from"./index
         message: 'There was a problem'
       }
     }
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.hover(canvas.getByRole('button'));
   }
 }`,...(b=(u=n.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};var m,g,l;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
   args: {
@@ -60,11 +66,5 @@ import{f as d,w as c,u as i}from"./index-PX7pnrjg.js";import{c as L}from"./index
     status: 'Done!',
     progress: 1,
     onInterrupt: fn()
-  },
-  play: async ({
-    canvasElement
-  }) => {
-    const canvas = within(canvasElement);
-    await userEvent.hover(canvas.getByRole('button'));
   }
 }`,...(B=(I=o.parameters)==null?void 0:I.docs)==null?void 0:B.source}}};const P=["Failed","Loading","InterruptLoading","InterruptError","Complete"];export{o as Complete,n as Failed,s as InterruptError,t as InterruptLoading,r as Loading,P as __namedExportsOrder,N as default};
