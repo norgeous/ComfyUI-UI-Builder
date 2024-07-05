62830e4409bfd4254823c279f7e59f0e78716027
norgeous <daniel@harte.me>
2024-07-05 16:19:55 +0100

    Update styled.js

--- src/components/App/styled.js
+++ src/components/App/styled.js
@@ -6,13 +6,11 @@ export const Page = styled.div`
   height: 100svh;
 
   // portrait first
-  /* grid-template-rows: minmax(50%, auto) minmax(auto, 400px); */
   grid-template-rows: auto 400px;
 
   // landscape overrides
   @media (min-aspect-ratio: 1/1) {
     grid-template-rows: none;
-    /* grid-template-columns: minmax(auto, 460px) minmax(50%, auto); */
     grid-template-columns: 460px auto;
     & > :first-child {
       order: 2; // change the order on landscape, so that sidebar is first
