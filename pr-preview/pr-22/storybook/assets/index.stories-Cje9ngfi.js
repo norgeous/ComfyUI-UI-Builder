951fce43ce725d0951d120d36f2d07a0e44e6d4b
norgeous <norgeous@users.noreply.github.com>
2024-07-05 11:11:59 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-Cje9ngfi.js
@@ -0,0 +1,3 @@
+const n=()=>"TIMELINE",t=n,a={title:"Timeline",component:t},e={args:{}};var s,r,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const i=["Timeline"];export{e as Timeline,i as __namedExportsOrder,a as default};
