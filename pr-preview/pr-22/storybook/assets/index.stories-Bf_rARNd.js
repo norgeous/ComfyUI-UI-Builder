8795cad0ae025439b965958d5357a9a99f4331d6
norgeous <norgeous@users.noreply.github.com>
2024-07-07 21:54:53 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-Bf_rARNd.js
@@ -0,0 +1,8 @@
+import{S as p}from"./SimpleStateDecorator-DWwLr0Lk.js";import{c as m}from"./index-DHxs-5M9.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-PX7pnrjg.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./InputRefContext-5kgal7hN.js";import"./index-BAs1Uaq-.js";import"./index-D3ylJrlI.js";import"./index-Drqau_A0.js";import"./index-Ckeq2DK0.js";import"./floating-ui.react-Dnqmk7Qj.js";import"./index-B8XB3FuZ.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-wo4bmeOh.js";import"./index-uIzakoab.js";import"./SpeechContext-CGWUamV5.js";import"./index-D0Aq9VeK.js";import"./index-DSSvUeEN.js";import"./index-Dul5t1_L.js";import"./index-CsZy_wK1.js";const q={title:"InputSpeech",component:m,decorators:[p]},t={args:{id:"demo",label:"My InputSpeech Label",info:"help text",defaultValue:""}};var o,r,e;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputSpeech Label',
+    info: 'help text',
+    defaultValue: ''
+  }
+}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const v=["InputSpeech"];export{t as InputSpeech,v as __namedExportsOrder,q as default};
---
+++ pr-preview/pr-22/storybook/assets/index.stories-DjHr5egq.js
@@ -0,0 +1,13 @@
+import{c as p}from"./index-DjpIZONJ.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D0Aq9VeK.js";import"./index-D3ylJrlI.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-BAMY2Nnw.js";import"./index-wo4bmeOh.js";import"./index-dHGaPU72.js";import"./index-DkOkptKI.js";import"./InputRefContext-5kgal7hN.js";import"./deepEqual-_sTZaZ0q.js";import"./index-DSSvUeEN.js";import"./index-Dul5t1_L.js";import"./index-uIzakoab.js";import"./index-Ckeq2DK0.js";import"./floating-ui.react-Dnqmk7Qj.js";import"./index-B8XB3FuZ.js";import"./index-ThrsfEIw.js";import"./FormContext-BvePLfBF.js";import"./index-BAs1Uaq-.js";import"./index-Drqau_A0.js";import"./index-DO6smgbx.js";import"./index-BLcIA5e6.js";import"./index-DaB1NRcB.js";import"./index-D6TAio_k.js";import"./index-D-iMkCee.js";import"./index-BLrJbezy.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./index-CdEmoYaS.js";import"./index-CsZy_wK1.js";import"./index-KviudHZg.js";import"./index-DHxs-5M9.js";import"./SpeechContext-CGWUamV5.js";import"./index-4KqKx7EM.js";import"./index-CdGmMi-p.js";import"./index-AC3OeeP5.js";import"./styled-BAeofX6r.js";import"./index-D22ECEzf.js";import"./index-C-rCtLry.js";const T={title:"Sidebar",component:p},r={parameters:{layout:"fullscreen",screenshot:{viewport:{width:412,height:700}}},args:{}};var t,o,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen',
+    screenshot: {
+      viewport: {
+        width: 412,
+        // half the width of github README.md on desktop
+        height: 700
+      }
+    }
+  },
+  args: {}
+}`,...(i=(o=r.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const U=["Sidebar"];export{r as Sidebar,U as __namedExportsOrder,T as default};
