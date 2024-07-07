a0d00e7980b3e6958337e2e52ccc7e924cd795db
norgeous <norgeous@users.noreply.github.com>
2024-07-07 21:59:14 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-4XaHAIEM.js
@@ -0,0 +1,12 @@
+import{w as i,u as m}from"./index-PX7pnrjg.js";import{c as s}from"./index-CrhpMeTx.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./index-DmPTbrnN.js";import"./index-D3ylJrlI.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./SettingsContext-CrmNDnzl.js";import"./index-Cy-U9SvD.js";import"./index-D0Aq9VeK.js";import"./index-wo4bmeOh.js";import"./index-gWt6HHhl.js";import"./index-KviudHZg.js";import"./index-BxrFAvcI.js";import"./index-Ckeq2DK0.js";import"./floating-ui.react-Dnqmk7Qj.js";import"./index-B8XB3FuZ.js";import"./styled-BVZT0ieL.js";const z={title:"MainContent",component:s},t={parameters:{layout:"fullscreen"},args:{},play:async({canvasElement:a})=>{const e=i(a);await m.click(e.getByRole("button"))}};var r,o,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  args: {},
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('button'));
+  }
+}`,...(n=(o=t.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const A=["MainContent"];export{t as MainContent,A as __namedExportsOrder,z as default};
---
+++ pr-preview/pr-22/storybook/assets/index.stories-DLr6zl1C.js
@@ -0,0 +1,22 @@
+import{f as l}from"./index-PX7pnrjg.js";import{S as i}from"./SimpleStateDecorator-Bp7xPB8C.js";import{c as a}from"./index-D6lL8tdI.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./InputRefContext-5kgal7hN.js";import"./index-udUPDOx1.js";import"./index-D3ylJrlI.js";import"./index-Drqau_A0.js";import"./index-Ckeq2DK0.js";import"./floating-ui.react-Dnqmk7Qj.js";import"./index-B8XB3FuZ.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-wo4bmeOh.js";import"./index-BxrFAvcI.js";import"./index-WZ3keo1w.js";import"./deepEqual-_sTZaZ0q.js";import"./index-D0Aq9VeK.js";import"./index-99oPS3m_.js";import"./index-DUGlproB.js";import"./index-D8HYKQ7O.js";import"./index-KviudHZg.js";const E={title:"InputFile",component:a,decorators:[i]},t={args:{id:"demo",label:"My InputFile Label",info:"help text",defaultValue:"https://placehold.co/600x400",options:[{label:"Option 1",value:"https://placehold.co/600x400"},{label:"Option 2",value:"https://placehold.co/600x500"},{label:"Option 3",value:"https://placehold.co/600x600"},{label:"Option X",value:"/view?type=input&filename=filename.jpg"}],onFileUpload:l()}};var o,e,p;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputFile Label',
+    info: 'help text',
+    defaultValue: 'https://placehold.co/600x400',
+    options: [{
+      label: 'Option 1',
+      value: 'https://placehold.co/600x400'
+    }, {
+      label: 'Option 2',
+      value: 'https://placehold.co/600x500'
+    }, {
+      label: 'Option 3',
+      value: 'https://placehold.co/600x600'
+    }, {
+      label: 'Option X',
+      value: \`/view?type=input&filename=filename.jpg\`
+    }],
+    onFileUpload: fn()
+  }
+}`,...(p=(e=t.parameters)==null?void 0:e.docs)==null?void 0:p.source}}};const k=["InputFile"];export{t as InputFile,k as __namedExportsOrder,E as default};
