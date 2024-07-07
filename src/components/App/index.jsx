05010d44405d72ab938e221f59628b32a2d2345f
norgeous <daniel@harte.me>
2024-07-07 23:26:12 +0100

    move header, attempting to make it more applicationy

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,16 @@
 import MainContent from '@/components/MainContent';
+import Layout from '@/components/Layout';
+import FormHeader from '@/components/FormHeader';
 import Sidebar from '@/components/Sidebar';
 import StatusBar from '@/components/StatusBar';
 import { Content, Page } from './styled';
 
 const App = () => (
   <Page>
+    <Layout as="header" column bgfg={2}>
+      <FormHeader />
+    </Layout>
+
     <Content>
       <MainContent />
       <Sidebar />
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,14 +1,9 @@
 import Layout from '@/components/Layout';
-import FormHeader from '@/components/FormHeader';
 import FormBuilder from '@/components/FormBuilder';
 import FormControls from '@/components/FormControls';
 
 const Sidebar = () => (
   <Layout as="aside" column fullHeight bgfg={1}>
-    <Layout as="header" column bgfg={2}>
-      <FormHeader />
-    </Layout>
-
     <FormBuilder />
 
     <Layout as="footer" pad column gap="md" bgfg={2}>
