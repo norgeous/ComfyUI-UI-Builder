9df5507fe78a76422e22aff35ab62eb24e0248b3
norgeous <norgeous@users.noreply.github.com>
2024-07-08 09:57:21 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-Dmk-nlBH.js
@@ -0,0 +1,24 @@
+import{w as d,u as c}from"./index-PX7pnrjg.js";import{c as b}from"./index-DW-ESDL0.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./index-D0Aq9VeK.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-wo4bmeOh.js";import"./index-KviudHZg.js";import"./index-CRfdTz-P.js";import"./index-Ckeq2DK0.js";import"./floating-ui.react-Dnqmk7Qj.js";import"./index-B8XB3FuZ.js";import"./styled-BVZT0ieL.js";const k={title:"QueueItem",component:b},e={args:{promptId:"baadb00b-babe-462f-cafe-d34dd00dcafe",type:"progress",node:"7",value:4,max:28},play:async({canvasElement:p})=>{const i=d(p);await c.hover(i.getByRole("button"))}},a={args:{promptId:"baadb00b-babe-462f-cafe-d34dd00dcafe",type:"executing",node:null,value:28,max:28,images:["/view?filename=01"]}};var t,o,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    promptId: 'baadb00b-babe-462f-cafe-d34dd00dcafe',
+    type: 'progress',
+    node: '7',
+    value: 4,
+    max: 28
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.hover(canvas.getByRole('button'));
+  }
+}`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};var n,s,m;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    promptId: 'baadb00b-babe-462f-cafe-d34dd00dcafe',
+    type: 'executing',
+    node: null,
+    value: 28,
+    max: 28,
+    images: ['/view?filename=01']
+  }
+}`,...(m=(s=a.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};const q=["Loading","Complete"];export{a as Complete,e as Loading,q as __namedExportsOrder,k as default};
