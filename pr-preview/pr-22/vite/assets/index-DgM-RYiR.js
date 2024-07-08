daa48a6489c934a613a7d99bf32acd726b9a98b7
norgeous <norgeous@users.noreply.github.com>
2024-07-08 10:04:39 +0000

    Deploy preview for PR 22 🛫

--- pr-preview/pr-22/vite/assets/index-DgM-RYiR.js
+++ pr-preview/pr-22/vite/assets/index-DgM-RYiR.js
@@ -735,12 +735,12 @@ Error generating stack: `+V.message+`
   display: grid;
 
   // portrait first
-  grid-template-rows: auto 400px;
+  grid-template-rows: minmax(50%, auto) minmax(auto, 400px);
 
   // landscape overrides
   @media (min-aspect-ratio: 1/1) {
     grid-template-rows: none;
-    grid-template-columns: 460px auto;
+    grid-template-columns: minmax(auto, 460px) minmax(50%, auto);
     & > :first-child {
       order: 2; // change the order on landscape, so that sidebar is first
     }
