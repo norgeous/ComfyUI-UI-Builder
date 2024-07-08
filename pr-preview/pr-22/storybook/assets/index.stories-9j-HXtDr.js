c9696d956a7b5f681ccdfed897891d29e64c7a32
norgeous <norgeous@users.noreply.github.com>
2024-07-08 12:09:04 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-9j-HXtDr.js
@@ -0,0 +1,13 @@
+import{c as p}from"./index-CA5dvPAM.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D0Aq9VeK.js";import"./index-D3ylJrlI.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-BAMY2Nnw.js";import"./index-wo4bmeOh.js";import"./index-PNfHRmpP.js";import"./FormContext-BvePLfBF.js";import"./InputRefContext-5kgal7hN.js";import"./index-BJht4iMF.js";import"./index-Drqau_A0.js";import"./index-Ckeq2DK0.js";import"./floating-ui.react-Dnqmk7Qj.js";import"./index-B8XB3FuZ.js";import"./index-CRfdTz-P.js";import"./index-DO6smgbx.js";import"./index-sSW-04aA.js";import"./deepEqual-_sTZaZ0q.js";import"./index-BQ1uf3uC.js";import"./index-B5VMmXvS.js";import"./index-DtrQI2xG.js";import"./index-Cn8bdcsH.js";import"./index-CCQbKop9.js";import"./index-B_0KnK0B.js";import"./index-DHvLdZ8A.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./index-TFdrJtFz.js";import"./index-BFYRQwOv.js";import"./index-KviudHZg.js";import"./index-B96IRPPP.js";import"./SpeechContext-CGWUamV5.js";import"./index-DnegYAD_.js";import"./index-CdGmMi-p.js";import"./index-WfL3FixB.js";import"./styled-BAeofX6r.js";import"./index-g7eq_-Rx.js";import"./index-C2JW72b0.js";const Q={title:"Sidebar",component:p},r={parameters:{layout:"fullscreen",screenshot:{viewport:{width:412,height:700}}},args:{}};var t,o,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen',
+    screenshot: {
+      viewport: {
+        width: 412,
+        // half the width of github README.md on desktop
+        height: 700
+      }
+    }
+  },
+  args: {}
+}`,...(i=(o=r.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const T=["Sidebar"];export{r as Sidebar,T as __namedExportsOrder,Q as default};
