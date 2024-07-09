8d9eb44abd1f8af74fee50f7b417256afb8bcbf6
norgeous <daniel@harte.me>
2024-07-09 15:17:02 +0100

    Update useComfyUpload.js

--- packages/comfybridge/useComfyUpload.js
+++ packages/comfybridge/useComfyUpload.js
@@ -1,19 +1,19 @@
-import useComfyApi from './queue/useComfyApi';
+// import useComfyApi from './queue/useComfyApi';
 
-const useComfyUpload = ({ file, enabled, onComplete }) => {
-  const formData = new FormData();
-  formData.append('image', file);
+// const useComfyUpload = ({ file, enabled, onComplete }) => {
+//   const formData = new FormData();
+//   formData.append('image', file);
 
-  return useComfyApi({
-    fetchUrl: '/upload/image',
-    options: {
-      method: 'POST',
-      body: formData,
-      headers: undefined,
-    },
-    enabled,
-    onComplete,
-  });
-};
+//   return useComfyApi({
+//     fetchUrl: '/upload/image',
+//     options: {
+//       method: 'POST',
+//       body: formData,
+//       headers: undefined,
+//     },
+//     enabled,
+//     onComplete,
+//   });
+// };
 
-export default useComfyUpload;
+// export default useComfyUpload;
