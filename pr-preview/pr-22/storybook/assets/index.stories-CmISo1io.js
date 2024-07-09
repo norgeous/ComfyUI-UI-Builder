a31a2afff7a90e824205c60492c92c1659d8ff91
norgeous <norgeous@users.noreply.github.com>
2024-07-09 13:15:17 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-CmISo1io.js
@@ -0,0 +1,16 @@
+import{w as s,u as i}from"./index-PX7pnrjg.js";import{c as p}from"./index-xzKNM7iM.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./FormContext-BvePLfBF.js";import"./index-wo4bmeOh.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-CRfdTz-P.js";import"./index-DL7FrFuT.js";import"./index-D3ylJrlI.js";import"./index-Dj0aUtmp.js";import"./index-B8XB3FuZ.js";import"./index-BK5_XIt8.js";const R={title:"DebugMenu",component:p},t={parameters:{screenshot:{viewport:{height:250}}},args:{},play:async({canvasElement:n})=>{const a=s(n);await i.click(a.getByRole("button"))}};var r,e,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  parameters: {
+    screenshot: {
+      viewport: {
+        height: 250
+      }
+    }
+  },
+  args: {},
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('button'));
+  }
+}`,...(o=(e=t.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const O=["DebugMenu"];export{t as DebugMenu,O as __namedExportsOrder,R as default};
