57a378af6bfc04a663c803d69b2011f7a0538793
norgeous <norgeous@users.noreply.github.com>
2024-07-09 14:24:41 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/storybook/assets/index.stories-DkFN0ykm.js
@@ -0,0 +1,28 @@
+import{w as f,u as l}from"./index-PX7pnrjg.js";import{c as g}from"./index-BXKET7K1.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-D_h4Ovhh.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./index-D0Aq9VeK.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-wo4bmeOh.js";import"./index-KviudHZg.js";import"./index-CRfdTz-P.js";import"./index-Dl0_K7pp.js";import"./index-B8XB3FuZ.js";import"./styled-BVZT0ieL.js";const A={title:"QueueItem",component:g},e={args:{promptId:"baadb00b-babe-462f-cafe-d34dd00dcafe"}},a={args:{promptId:"baadb00b-babe-462f-cafe-d34dd00dcafe",type:"progress",node:"7",value:4,max:28},play:async({canvasElement:b})=>{const u=f(b);await l.hover(u.getByRole("button"))}},r={args:{promptId:"baadb00b-babe-462f-cafe-d34dd00dcafe",type:"executing",node:null,value:28,max:28,images:["/view?filename=01"]}};var o,t,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    promptId: 'baadb00b-babe-462f-cafe-d34dd00dcafe'
+  }
+}`,...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var s,d,p;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    promptId: 'baadb00b-babe-462f-cafe-d34dd00dcafe',
+    type: 'progress',
+    node: '7',
+    value: 4,
+    max: 28
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.hover(canvas.getByRole('button'));
+  }
+}`,...(p=(d=a.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var m,c,i;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    promptId: 'baadb00b-babe-462f-cafe-d34dd00dcafe',
+    type: 'executing',
+    node: null,
+    value: 28,
+    max: 28,
+    images: ['/view?filename=01']
+  }
+}`,...(i=(c=r.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const D=["Queued","Loading","Complete"];export{r as Complete,a as Loading,e as Queued,D as __namedExportsOrder,A as default};
