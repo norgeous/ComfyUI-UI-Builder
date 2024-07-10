44074f28d9bf6c95ecc706f65ef7aa8bddff264b
norgeous <norgeous@users.noreply.github.com>
2024-07-10 21:34:48 +0000

    Deploy preview for PR 22 🛫

--- pr-preview/pr-22/storybook/assets/index.stories-CQ78zvzk.js
+++
@@ -1,6 +0,0 @@
-import{c as m}from"./index-C_nMF3kN.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./index-By7clcRM.js";import"./index-D3ylJrlI.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./index-BAMY2Nnw.js";import"./SettingsContext-CrmNDnzl.js";import"./index-uCiBnAwm.js";import"./index-DhfeuN3W.js";import"./bgfg-iYkfKEl_.js";import"./index-0ZcNPAvE.js";import"./index-KviudHZg.js";import"./index-CvT9l5cF.js";import"./index-Dq9VOxrg.js";import"./index-BxibOwXx.js";import"./styled-BVZT0ieL.js";const b={title:"MainContent",component:m},r={parameters:{layout:"fullscreen"},args:{}};var t,o,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  parameters: {
-    layout: 'fullscreen'
-  },
-  args: {}
-}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const h=["MainContent"];export{r as MainContent,h as __namedExportsOrder,b as default};
