e0f42240acbb4bad42a989d7e8e497c9ca718401
norgeous <daniel@harte.me>
2024-07-10 16:45:54 +0100

    Update useImageSize.js

--- src/components/ImageGrid/useImageSize.js
+++ src/components/ImageGrid/useImageSize.js
@@ -4,6 +4,7 @@ const useImageSize = image => {
   const [size, setSize] = useState([512, 512]);
 
   useEffect(() => {
+    if (!image) return;
     const img = new Image();
     img.crossOrigin = 'anonymous';
     img.onload = () => setSize([img.naturalWidth, img.naturalHeight]);
