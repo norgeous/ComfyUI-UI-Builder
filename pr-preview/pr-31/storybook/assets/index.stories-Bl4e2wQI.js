11b5d26a5198090f97af641ac89275f4756c1179
norgeous <norgeous@users.noreply.github.com>
2024-07-13 23:22:09 +0000

    Remove preview for PR 31 🛬

--- pr-preview/pr-31/storybook/assets/index.stories-Bl4e2wQI.js
+++
@@ -1,8 +0,0 @@
-import{S as a}from"./SimpleStateDecorator-LKA-djQs.js";import{c as p}from"./index-COqoQUSX.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DFHy4GFg.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-BV3z3PqH.js";import"./InputRefContext-5kgal7hN.js";import"./index-DJTv0orh.js";import"./index-D3ylJrlI.js";import"./index-B980p5b5.js";import"./index-2ZZ_S3jo.js";import"./floating-ui.react-CPLfUUbp.js";import"./index-BxibOwXx.js";import"./styled-components.browser.esm-Cm6OPFyG.js";import"./bgfg-iYkfKEl_.js";import"./index-zXYQOjb1.js";import"./index-B3-Iaed7.js";import"./index-DV02L8s6.js";import"./index-_BNh20Fg.js";const h={title:"InputTextarea",component:p,decorators:[a]},t={args:{id:"demo",label:"My InputTextarea Label",info:"info text",defaultValue:""}};var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    id: 'demo',
-    label: 'My InputTextarea Label',
-    info: 'info text',
-    defaultValue: ''
-  }
-}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const j=["InputTextarea"];export{t as InputTextarea,j as __namedExportsOrder,h as default};
