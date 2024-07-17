612cc4c33194d569f5e9a5efa552a726aa8cec1d
norgeous <norgeous@users.noreply.github.com>
2024-07-17 21:03:21 +0000

    Remove preview for PR 32 🛬

--- pr-preview/pr-32/storybook/assets/index.stories-BKmFkCeO.js
+++
@@ -1,9 +0,0 @@
-import{S as r}from"./SimpleStateDecorator-Brl1p4D3.js";import{c as e}from"./index-MgEtv_w0.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BW_Q0FrA.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-BV3z3PqH.js";import"./InputRefContext-5kgal7hN.js";import"./index-CdaHVHIZ.js";import"./index-D3ylJrlI.js";import"./index-Brh1w6mr.js";import"./index-DCGSrkkW.js";import"./index-BxibOwXx.js";import"./styled-components.browser.esm-CgTwxB-0.js";import"./bgfg-BUc8g66r.js";import"./index-CgtIIt52.js";import"./index-BLrka4M9.js";import"./index-CihILjeG.js";import"./index-C6ZSdaCS.js";const E={title:"InputText",component:e,decorators:[r]},t={args:{id:"demo",label:"My InputText Label",info:"info text",options:["Option 1","Option 2","Option 3","Option 4","Option 5","Option 6"],defaultValue:""}};var o,p,i;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    id: 'demo',
-    label: 'My InputText Label',
-    info: 'info text',
-    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6'],
-    defaultValue: ''
-  }
-}`,...(i=(p=t.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const h=["InputText"];export{t as InputText,h as __namedExportsOrder,E as default};
