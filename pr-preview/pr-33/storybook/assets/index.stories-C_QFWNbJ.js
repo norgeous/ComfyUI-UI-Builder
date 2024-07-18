1bce18d642107a3b2ed7936a30549d676df97606
norgeous <norgeous@users.noreply.github.com>
2024-07-18 20:05:22 +0000

    Deploy preview for PR 33 🛫

---
+++ pr-preview/pr-33/storybook/assets/index.stories-C_QFWNbJ.js
@@ -0,0 +1,57 @@
+import{w as s,u as p}from"./index-BW_Q0FrA.js";import{S as b}from"./SimpleStateDecorator-B1kOQ4tw.js";import{c}from"./index-CtMxSbrI.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-BV3z3PqH.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./InputRefContext-5kgal7hN.js";import"./index-DfKYqAu0.js";import"./index-D3ylJrlI.js";import"./index-Brh1w6mr.js";import"./index-DCGSrkkW.js";import"./index-BxibOwXx.js";import"./styled-components.browser.esm-CgTwxB-0.js";import"./bgfg-BUc8g66r.js";import"./index-usF4-fxs.js";import"./index-BMMPm1Gq.js";import"./index-BLrka4M9.js";import"./index-BgG56ZXN.js";import"./index-B_Bqb2rN.js";import"./index-C9bZ1-4o.js";import"./deepEqual-_sTZaZ0q.js";const C={title:"InputNumberRange",component:c,decorators:[b]},e={args:{id:"demo",label:"My InputNumberRange Label",info:"info text",defaultValue:2,min:1,max:420,options:[{label:"1",value:1},{label:"2",value:2},{label:"3",value:3},{label:"4",value:4},{label:"5",value:5}]}},a={args:{id:"demo",label:"My InputNumberRange Label",info:"info text",defaultValue:2,min:1,max:420,options:[{label:"1",value:1},{label:"2",value:2},{label:"3",value:3},{label:"4",value:4},{label:"5",value:5}]},play:async({canvasElement:m})=>{const u=s(m);await p.click(u.getByRole("button"))}};var n,l,t;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputNumberRange Label',
+    info: 'info text',
+    defaultValue: 2,
+    min: 1,
+    max: 420,
+    options: [{
+      label: '1',
+      value: 1
+    }, {
+      label: '2',
+      value: 2
+    }, {
+      label: '3',
+      value: 3
+    }, {
+      label: '4',
+      value: 4
+    }, {
+      label: '5',
+      value: 5
+    }]
+  }
+}`,...(t=(l=e.parameters)==null?void 0:l.docs)==null?void 0:t.source}}};var o,r,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputNumberRange Label',
+    info: 'info text',
+    defaultValue: 2,
+    min: 1,
+    max: 420,
+    options: [{
+      label: '1',
+      value: 1
+    }, {
+      label: '2',
+      value: 2
+    }, {
+      label: '3',
+      value: 3
+    }, {
+      label: '4',
+      value: 4
+    }, {
+      label: '5',
+      value: 5
+    }]
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('button'));
+  }
+}`,...(i=(r=a.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const F=["Range","Number"];export{a as Number,e as Range,F as __namedExportsOrder,C as default};
---
+++ pr-preview/pr-33/storybook/assets/index.stories-STsoGRE2.js
@@ -0,0 +1,8 @@
+import{S as a}from"./SimpleStateDecorator-B1kOQ4tw.js";import{c as p}from"./index-Cuwff-Yo.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BW_Q0FrA.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-BV3z3PqH.js";import"./InputRefContext-5kgal7hN.js";import"./index-DfKYqAu0.js";import"./index-D3ylJrlI.js";import"./index-Brh1w6mr.js";import"./index-DCGSrkkW.js";import"./index-BxibOwXx.js";import"./styled-components.browser.esm-CgTwxB-0.js";import"./bgfg-BUc8g66r.js";import"./index-usF4-fxs.js";import"./index-BLrka4M9.js";import"./index-BgG56ZXN.js";import"./index-B_Bqb2rN.js";const O={title:"InputTextarea",component:p,decorators:[a]},t={args:{id:"demo",label:"My InputTextarea Label",info:"info text",defaultValue:""}};var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    id: 'demo',
+    label: 'My InputTextarea Label',
+    info: 'info text',
+    defaultValue: ''
+  }
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const h=["InputTextarea"];export{t as InputTextarea,h as __namedExportsOrder,O as default};
