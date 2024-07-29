2eb058dffd074e1dbe531bd10fe84334713a2414
norgeous <norgeous@users.noreply.github.com>
2024-07-29 15:45:36 +0000

    Deploy preview for PR 33 🛫

--- pr-preview/pr-33/storybook/assets/index.stories-hFdzS3KD.js
+++
@@ -1,9 +0,0 @@
-import{S as i}from"./SimpleStateDecorator-D0dCArzT.js";import{c as p}from"./index-GJo_aLIV.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BW_Q0FrA.js";import"./index-BbP3371Q.js";import"./index-4V56_mca.js";import"./index-BAMY2Nnw.js";import"./index-BV3z3PqH.js";import"./InputRefContext-5kgal7hN.js";import"./index-CpAKrSB3.js";import"./index-D3ylJrlI.js";import"./index-C7f4sYud.js";import"./index-zBpH53_J.js";import"./index-DxjWwZXO.js";import"./styled-components.browser.esm-CgTwxB-0.js";import"./bgfg-BUc8g66r.js";import"./index-CjMGYxTR.js";import"./index-BLrka4M9.js";import"./index-BzafusQ9.js";import"./index-1Pc6khdR.js";import"./SpeechContext-CGWUamV5.js";import"./index-D1HvzO9f.js";const E={title:"InputText",component:p,decorators:[i]},t={args:{id:"demo",label:"My InputText Label",info:"info text",defaultValue:"",controlOptions:["weight","speech","limit"]}};var o,r,e;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    id: 'demo',
-    label: 'My InputText Label',
-    info: 'info text',
-    defaultValue: '',
-    controlOptions: ['weight', 'speech', 'limit']
-  }
-}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const j=["InputText"];export{t as InputText,j as __namedExportsOrder,E as default};
