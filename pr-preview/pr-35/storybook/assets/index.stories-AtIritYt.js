ba2e9479ce8ff08e244539cf8947cf5366e90687
norgeous <norgeous@users.noreply.github.com>
2024-08-11 22:22:02 +0000

    Deploy preview for PR 35 🛫

---
+++ pr-preview/pr-35/storybook/assets/index.stories-AtIritYt.js
@@ -0,0 +1,13 @@
+import{c as p}from"./index-DMmXo8SC.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BLrka4M9.js";import"./index-D3ylJrlI.js";import"./styled-components.browser.esm-CgTwxB-0.js";import"./index-BAMY2Nnw.js";import"./bgfg-BUc8g66r.js";import"./index-CXTvAVDq.js";import"./FormContext-BvePLfBF.js";import"./InputRefContext-5kgal7hN.js";import"./index-79BaiYNo.js";import"./index-_a3JP2zR.js";import"./index-BfpgPM-d.js";import"./index-DxjWwZXO.js";import"./index-CjMGYxTR.js";import"./index-DO6smgbx.js";import"./index-BQvUQ413.js";import"./deepEqual-_sTZaZ0q.js";import"./index-t7wRVlRJ.js";import"./index-BA0FdTzk.js";import"./index-DPjZG4HV.js";import"./index-DIQLdyYk.js";import"./index-ButkP6eG.js";import"./ComfyBridgeContext-CTMRF_Mz.js";import"./index-C--OF_WR.js";import"./SpeechContext-CGWUamV5.js";import"./index-D1HvzO9f.js";import"./index-CfUGCC_c.js";import"./index-DTXrJjgA.js";import"./index-DCti1NPa.js";import"./index-yHnN2X2S.js";import"./styles-DJZsKyOI.js";import"./index-CVrxB1Z5.js";import"./index-COq2LqQp.js";import"./index-WpBOhxnB.js";import"./ConfigsContext-Cp8R_4Vo.js";const N={title:"Sidebar",component:p},r={parameters:{layout:"fullscreen",screenshot:{viewport:{width:412,height:700}}},args:{}};var t,o,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen',
+    screenshot: {
+      viewport: {
+        width: 412,
+        // half the width of github README.md on desktop
+        height: 700
+      }
+    }
+  },
+  args: {}
+}`,...(i=(o=r.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const P=["Sidebar"];export{r as Sidebar,P as __namedExportsOrder,N as default};
