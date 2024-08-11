14babb03c47a77c28607f12558797468838fe0cf
norgeous <norgeous@users.noreply.github.com>
2024-08-11 22:05:07 +0000

    Remove preview for PR 34 🛬

--- pr-preview/pr-34/storybook/assets/index.stories-D4dF-0mI.js
+++
@@ -1,6 +0,0 @@
-import{c as m}from"./index-5odAx3h5.js";import"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./ConfigsContext-Cp8R_4Vo.js";import"./index-BLrka4M9.js";import"./index-D3ylJrlI.js";import"./styled-components.browser.esm-CgTwxB-0.js";import"./index-BAMY2Nnw.js";import"./bgfg-BUc8g66r.js";import"./index-BQvUQ413.js";import"./InputRefContext-5kgal7hN.js";import"./deepEqual-_sTZaZ0q.js";import"./index-t7wRVlRJ.js";import"./index-BA0FdTzk.js";import"./index-CjMGYxTR.js";import"./index-BfpgPM-d.js";import"./index-DxjWwZXO.js";import"./index-79BaiYNo.js";import"./index-_a3JP2zR.js";const h={title:"FormHeader",component:m},r={parameters:{layout:"fullscreen"},args:{}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  parameters: {
-    layout: 'fullscreen'
-  },
-  args: {}
-}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const j=["FormHeader"];export{r as FormHeader,j as __namedExportsOrder,h as default};
